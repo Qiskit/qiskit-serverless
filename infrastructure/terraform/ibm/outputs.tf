dd51fdf625c94defc39bd6e91242a33379c396dd
david <9059044+Tansito@users.noreply.github.com>
2022-09-28 20:35:36 +0200

    Commented the outputs due to the cluster changes

--- infrastructure/terraform/ibm/outputs.tf
+++ infrastructure/terraform/ibm/outputs.tf
@@ -2,19 +2,19 @@
 # IKS on VPC Outputs
 ##############################################################################
 
-output cluster_public_endpoint {
-    description = "Public end-poinft of the created cluster"
-    value       = "${ibm_container_vpc_cluster.cluster.public_service_endpoint_url}"
-}
+# output cluster_public_endpoint {
+#     description = "Public end-poinft of the created cluster"
+#     value       = "${ibm_container_vpc_cluster.cluster.public_service_endpoint_url}"
+# }
 
-output cluster_name {
-    description = "Name of the IKS on VPC Cluster, only returns after cluster deploys"
-    value       = "${ibm_container_vpc_cluster.cluster.name}"
-}
+# output cluster_name {
+#     description = "Name of the IKS on VPC Cluster, only returns after cluster deploys"
+#     value       = "${ibm_container_vpc_cluster.cluster.name}"
+# }
 
-output cluster_id {
-    description = "ID of the IKS on VPC Cluster"
-    value       = "${ibm_container_vpc_cluster.cluster.id}"
-}
+# output cluster_id {
+#     description = "ID of the IKS on VPC Cluster"
+#     value       = "${ibm_container_vpc_cluster.cluster.id}"
+# }
 
 ##############################################################################
