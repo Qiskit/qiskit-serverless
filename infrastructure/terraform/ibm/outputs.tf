7915022bef75f2df0d57084ce28df20604d62a2e
david <9059044+Tansito@users.noreply.github.com>
2022-09-29 12:23:15 +0200

    Changed the output to support the new module

--- infrastructure/terraform/ibm/outputs.tf
+++ infrastructure/terraform/ibm/outputs.tf
@@ -2,19 +2,9 @@
 # IKS on VPC Outputs
 ##############################################################################
 
-# output cluster_public_endpoint {
-#     description = "Public end-poinft of the created cluster"
-#     value       = "${ibm_container_vpc_cluster.cluster.public_service_endpoint_url}"
-# }
-
-# output cluster_name {
-#     description = "Name of the IKS on VPC Cluster, only returns after cluster deploys"
-#     value       = "${ibm_container_vpc_cluster.cluster.name}"
-# }
-
-# output cluster_id {
-#     description = "ID of the IKS on VPC Cluster"
-#     value       = "${ibm_container_vpc_cluster.cluster.id}"
-# }
+output cluster_id {
+    description = "ID of the IKS on VPC Cluster"
+    value       = "${module.vpc_kubernetes_cluster.kubernetes_vpc_cluster_id}"
+}
 
 ##############################################################################
