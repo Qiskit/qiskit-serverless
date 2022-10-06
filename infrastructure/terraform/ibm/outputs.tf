732508df83c4faa5a9df92091b69b2ba9d5b0b90
javier.juarez <Javier.Juarez.Martinez@ibm.com>
2022-10-06 11:47:29 +0200

    chore: tflint

--- infrastructure/terraform/ibm/outputs.tf
+++ infrastructure/terraform/ibm/outputs.tf
@@ -2,9 +2,9 @@
 # IKS on VPC Outputs
 ##############################################################################
 
-output cluster_id {
-    description = "ID of the IKS on VPC Cluster"
-    value       = "${module.vpc_kubernetes_cluster.kubernetes_vpc_cluster_id}"
+output "cluster_id" {
+  description = "ID of the IKS on VPC Cluster"
+  value       = module.vpc_kubernetes_cluster.kubernetes_vpc_cluster_id
 }
 
 ##############################################################################
