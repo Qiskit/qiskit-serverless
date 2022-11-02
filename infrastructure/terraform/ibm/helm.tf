b9d07d7742df25ac86854a9d7f6eb92390e62961
David <9059044+Tansito@users.noreply.github.com>
2022-11-02 15:21:18 +0100

    Issue 40| Unify terraform helm configuration (#46)
    * Move terraform to values
    
    * Update terraform helm values
    
    * Add helm support in IBM Cloud

--- infrastructure/terraform/ibm/helm.tf
+++ infrastructure/terraform/ibm/helm.tf
@@ -2,16 +2,16 @@
 # Create a helm execution
 ##############################################################################
 
-# resource helm_release quantum_severless_helm {
-#   depends_on = [
-#     ibm_container_vpc_cluster.cluster
-#   ]
-#   name       = "${var.helm_name}"
-#   chart      = "${var.helm_path}"
+resource helm_release quantum_severless_helm {
+  depends_on = [
+    ibm_container_vpc_cluster.cluster
+  ]
+  name       = "${var.helm_name}"
+  chart      = "${var.helm_path}"
 
-#   values = [
-#     "${file("${var.values_file}")}"
-#   ]
-# }
+  values = [
+    "${file("${var.values_file}")}"
+  ]
+}
 
 ##############################################################################
