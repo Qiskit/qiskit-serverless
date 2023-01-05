0c20442e78ff5b620d09ad9582a072c677e7e485
Iskandar Sitdikov <IceKhan13@users.noreply.github.com>
2023-01-05 10:13:59 -0500

    Issue 96 | Client: ability to stop jobs (#101)
    * Issue 96 | Client: ability to stop jobs

--- client/tests/core/test_program.py
+++ client/tests/core/test_program.py
@@ -63,3 +63,4 @@ def test_program():
         assert "42" in recovered_job.logs()
         assert recovered_job.status().is_terminal()
         assert recovered_job.status() == JobStatus.SUCCEEDED
+        assert isinstance(job.stop(), bool)
