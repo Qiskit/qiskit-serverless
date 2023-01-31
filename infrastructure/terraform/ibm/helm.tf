3ae6367581f9600b5a5a16bc387589ca9895bcf8
david <9059044+Tansito@users.noreply.github.com>
2023-01-31 11:32:21 +0100

    Improved naming for helm release

--- infrastructure/terraform/ibm/helm.tf
+++ infrastructure/terraform/ibm/helm.tf
@@ -2,8 +2,8 @@
 # Create a helm execution
 ##############################################################################
 
-resource "helm_release" "quantum_serverless_chart" {
-  name       = "quantum-serverless-chart"
+resource "helm_release" "quantum_serverless_release" {
+  name       = "quantum-serverless-release"
   chart      = "../../helm/quantumserverless"
 
   values = [
