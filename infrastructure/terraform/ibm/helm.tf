32afe59172b6a2e2088277556fd0d95a980e1a8f
david <9059044+Tansito@users.noreply.github.com>
2023-01-31 11:29:15 +0100

    Fix data provider to obtain the certificate

--- infrastructure/terraform/ibm/helm.tf
+++ infrastructure/terraform/ibm/helm.tf
@@ -2,15 +2,10 @@
 # Create a helm execution
 ##############################################################################
 
-resource helm_release quantum_serverless_helm {
-  #name       = "${var.helm_name}"
+resource "helm_release" "quantum_serverless_chart" {
   name       = "quantum-serverless-chart"
-  #chart      = "${var.helm_path}"
   chart      = "../../helm/quantumserverless"
 
-  #values = [
-  #  "${file("${var.values_file}")}"
-  #]
   values = [
     file("values.yaml")
   ]
