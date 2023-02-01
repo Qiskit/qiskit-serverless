201d9104cedb96b0309b484aef177a54ef78fc47
david <9059044+Tansito@users.noreply.github.com>
2023-02-01 17:48:39 +0100

    Initial configuration for the repository project

---
+++ repository/api/views.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from .models import NestedProgram
+from .serializers import NestedProgramSerializer
+
+
+class NestedProgramViewSet(viewsets.ModelViewSet):
+    """
+    TODO: documentation here
+    """
+    queryset = NestedProgram.objects.all().order_by('created')
+    serializer_class = NestedProgramSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
