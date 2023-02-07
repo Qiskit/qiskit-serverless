37df515e03f7bd1a12e6b18da71d2186dc4e15ae
David <9059044+Tansito@users.noreply.github.com>
2023-02-07 16:04:40 +0100

    Repository: custom validators (#169)

--- repository/api/serializers.py
+++ repository/api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import NestedProgram
+from .validators import list_validator, dict_validator
 
 
 class NestedProgramSerializer(serializers.ModelSerializer):
@@ -9,3 +10,7 @@ class NestedProgramSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = NestedProgram
+        validators = [
+            list_validator.ListValidator(fields=["dependencies", "tags"], nullable=True),
+            dict_validator.DictValidator(fields=["env_vars", "arguments"], nullable=True)
+        ]
---
+++ repository/api/validators/dict_validator.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+from typing import Any, Dict, List, OrderedDict, Union
+
+
+class DictValidator:
+    """
+    TODO: description
+    """
+
+    def __init__(self, fields: List[str], nullable=True):
+        self.fields = fields
+        self.nullable = nullable
+
+    def __call__(self, attrs: OrderedDict[str, Any]):
+        error_messages = {}
+
+        for field in self.fields:
+            error_message = self.validate(field, attrs[field])
+            if error_message is not None:
+                error_messages.update(error_message)
+
+        if error_messages:
+            raise serializers.ValidationError(error_messages)
+
+    def validate(self, field: str, value: Any) -> Union[Dict[str, str], None]:
+        if value is None:
+            if not self.nullable:
+                return {f"{field}": "This field may not be null."}
+        else:
+            # Using `type` instead of `isinstance` to validate that it is a dict and no a subtype
+            value_type = type(value)
+            if value_type is not dict:
+                return {f"{field}": "This field must be a valid dict."}
+        return None
