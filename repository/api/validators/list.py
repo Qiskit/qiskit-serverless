d37b48209de5c2ec0e914efd2473e21c5e090ad5
david <9059044+Tansito@users.noreply.github.com>
2023-02-07 11:36:07 +0100

    Improved error visualization

--- repository/api/validators/list.py
+++ repository/api/validators/list.py
@@ -18,11 +18,9 @@ class List:
     def validate(self, field, value):
         if value is None:
             if self.nullable is False:
-                raise serializers.ValidationError(f"{field} field cannot be null.")
+                raise serializers.ValidationError({f"{field}": "This field may not be null."})
         else:
             # Using `type` instead of `isinstance` to validate that it is a list and no a subtype
             value_type = type(value)
             if value_type is not list:
-                raise serializers.ValidationError(f"{field} field must be a valid list.")
-
-
+                raise serializers.ValidationError({f"{field}": "This field must be a valid list."})
