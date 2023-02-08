26448ed1aa5ca9641081423f2699da71b52c8c60
david <9059044+Tansito@users.noreply.github.com>
2023-02-08 16:24:20 +0100

    Updated documentation for validators

--- repository/api/validators/list_validator.py
+++ repository/api/validators/list_validator.py
@@ -1,10 +1,19 @@
-from rest_framework import serializers
+"""
+List validator to be used in the serializers.
+"""
+
 from typing import Any, Dict, List, OrderedDict, Union
+from rest_framework import serializers
 
 
 class ListValidator:
     """
-    TODO: description
+    This validator checks if a specific set of fields contains:
+        - A value of type `list`
+        - If the value can be `null` or not
+
+    :param fields: set of fields that will be checked by this validator
+    :param nullable: specifies if the validator may accept `null` values
     """
 
     def __init__(self, fields: List[str], nullable=True):
@@ -23,6 +32,17 @@ class ListValidator:
             raise serializers.ValidationError(error_messages)
 
     def validate(self, field: str, value: Any) -> Union[Dict[str, str], None]:
+        """
+        Method that checks if the value of a specific field is a valid `list`.
+
+        :param field: name of the model attribute
+        :param value: field content to be checked
+        :return:
+            an error message of type:
+                `{ f"{field}": f"{error_message}" }`
+            if the value is not valid
+        """
+
         if value is None:
             if not self.nullable:
                 return {f"{field}": "This field may not be null."}
