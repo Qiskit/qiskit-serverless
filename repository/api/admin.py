133e8eefbe350c3409076d4ba09f2cbc62f89cef
david <9059044+Tansito@users.noreply.github.com>
2023-02-08 15:49:34 +0100

    Register nested program for admin panel

--- repository/api/admin.py
+++ repository/api/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
+"""
+Register the models for the admin panel.
+"""
 
-# Register your models here.
+from django.contrib import admin
+from .models import NestedProgram
+
+admin.site.register(NestedProgram)
