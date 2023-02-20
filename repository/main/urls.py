37477422327132964259768771e125e0914a36cb
Iskandar Sitdikov <iskandar.sitdikov@gmail.com>
2023-02-20 14:46:39 -0500

    Issue 201 | Repository: swagger / openapi schema

--- repository/main/urls.py
+++ repository/main/urls.py
@@ -17,12 +17,36 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
+schema = get_schema_view(  # pylint: disable=invalid-name
+    openapi.Info(
+        title="Program repository API",
+        default_version="v1",
+        description="List of available API endpoint for Program repository.",
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     re_path(r"^v1/api/", include(("api.v1.urls", "api"), namespace="v1")),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    # docs
+    re_path(
+        r"^swagger(?P<format>\.json|\.yaml)$",
+        schema.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    re_path(
+        r"^swagger/$",
+        schema.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    re_path(r"^redoc/$", schema.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
 
 if settings.DEBUG:
