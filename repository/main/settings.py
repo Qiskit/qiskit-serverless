a9e177873e19b6fc203ecacbe1ad5695f31502ae
Paul S. Schweigert <paul@paulschweigert.com>
2023-03-27 12:54:27 -0400

    I sometimes mispell words
    Signed-off-by: Paul S. Schweigert <paul@paulschweigert.com>

--- repository/main/settings.py
+++ repository/main/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "djano_prometheus.middleware.PrometheusAfterMiddleware",
+    "django_prometheus.middleware.PrometheusAfterMiddleware",
 ]
 
 ROOT_URLCONF = "main.urls"
