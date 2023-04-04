da932dedfe9f10053dcf0363c774d91a7cb416f3
david <9059044+Tansito@users.noreply.github.com>
2023-04-04 14:45:51 +0200

    Enable gateway static files for admin panel

--- gateway/main/settings.py
+++ gateway/main/settings.py
@@ -142,6 +142,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
