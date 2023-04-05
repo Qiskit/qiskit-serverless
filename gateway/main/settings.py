3ac0e1659d2474a78ac6ff735e513e4c4955e38c
david <9059044+Tansito@users.noreply.github.com>
2023-04-05 11:19:48 +0200

    Configured static and media for both projects

--- gateway/main/settings.py
+++ gateway/main/settings.py
@@ -144,6 +144,9 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
+MEDIA_URL = "media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- repository/main/settings.py
+++ repository/main/settings.py
@@ -129,8 +129,8 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
+MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-MEDIA_URL = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
