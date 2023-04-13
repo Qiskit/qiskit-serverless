842958c2165a72e7ccb2c8a6422bf27f44849585
david <9059044+Tansito@users.noreply.github.com>
2023-04-13 11:11:43 +0200

    Repository: updated tests with program

--- repository/tests/core/test_v1_program.py
+++ repository/tests/core/test_v1_program.py
@@ -1,6 +1,5 @@
 import json
 import os.path
-from pathlib import Path
 
 from django.contrib.auth.models import User
 from django.core.files import File
@@ -11,58 +10,58 @@ from rest_framework.test import APITestCase
 from api.models import Program
 
 
-class QuantumFunctionTests(APITestCase):
+class ProgramTests(APITestCase):
     fixtures = ["tests/fixtures/initial_data.json"]
 
-    def test_get_quantum_function_returns_200(self):
+    def test_get_program_returns_200(self):
         """
-        Retrieve information about a specific quantum function
+        Retrieve information about a specific program
         """
-        quantum_function_id = "1a7947f9-6ae8-4e3d-ac1e-e7d608deec82"
-        url = reverse("v1:quantum-functions-detail", args=[quantum_function_id])
+        program_id = "1a7947f9-6ae8-4e3d-ac1e-e7d608deec82"
+        url = reverse("v1:programs-detail", args=[program_id])
         response = self.client.get(url, format="json")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_get_non_created_quantum_function_returns_404(self):
+    def test_get_non_created_program_returns_404(self):
         """
-        Retrieve information about a specific quantum function that doesn't exist returns a 404
+        Retrieve information about a specific program that doesn't exist returns a 404
         """
-        url = reverse("v1:quantum-functions-detail", args=[2])
+        url = reverse("v1:programs-detail", args=[2])
         response = self.client.get(url, format="json")
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
-    def test_create_quantum_function_unauthenticated_returns_403(self):
+    def test_create_program_unauthenticated_returns_403(self):
         """
-        Create a quantum function without being authenticated returns a 403
+        Create a program without being authenticated returns a 403
         """
-        quantum_function_input = {}
+        program_input = {}
 
-        url = reverse("v1:quantum-functions-list")
-        response = self.client.post(url, data=quantum_function_input, format="json")
+        url = reverse("v1:programs-list")
+        response = self.client.post(url, data=program_input, format="json")
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
-    def test_create_quantum_function_with_empty_object_validation(self):
+    def test_create_program_with_empty_object_validation(self):
         """
-        Create a quantum function with an empty object as input should return a validation error
+        Create a program with an empty object as input should return a validation error
         """
-        quantum_function_input = {}
+        program_input = {}
         fields_to_check = ["title", "entrypoint", "artifact"]
         test_user = User.objects.get(username="test_user")
 
         self.client.force_login(test_user)
 
-        url = reverse("v1:quantum-functions-list")
-        response = self.client.post(url, data=quantum_function_input, format="json")
+        url = reverse("v1:programs-list")
+        response = self.client.post(url, data=program_input, format="json")
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
         failed_validation_fields_list = list(response.json().keys())
         self.assertListEqual(failed_validation_fields_list, fields_to_check)
 
-    def test_create_quantum_function_with_blank_values_validation(self):
+    def test_create_program_with_blank_values_validation(self):
         """
-        Create a quantum function with an object with blank values should return a validation error
+        Create a program with an object with blank values should return a validation error
         """
-        quantum_function_input = {
+        program_input = {
             "title": "",
             "description": "",
             "entrypoint": "",
@@ -79,21 +78,21 @@ class QuantumFunctionTests(APITestCase):
 
         self.client.force_login(test_user)
 
-        url = reverse("v1:quantum-functions-list")
-        response = self.client.post(url, data=quantum_function_input, format="json")
+        url = reverse("v1:programs-list")
+        response = self.client.post(url, data=program_input, format="json")
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
         failed_validation_fields_list = list(response.json().keys())
         self.assertListEqual(failed_validation_fields_list, fields_to_check)
 
-    def test_create_quantum_function_returns_201(self):
+    def test_create_program_returns_201(self):
         """
-        Create a quantum function
+        Create a program
         """
-        quantum_function_input = {
-            "title": "Awesome quantum function",
-            "description": "Awesome quantum function description",
-            "entrypoint": "quantum_function.py",
+        program_input = {
+            "title": "Awesome program",
+            "description": "Awesome program description",
+            "entrypoint": "program.py",
             "working_dir": "./",
             "version": "0.0.1",
             "env_vars": json.dumps({"DEBUG": True}),
@@ -106,7 +105,7 @@ class QuantumFunctionTests(APITestCase):
 
         self.client.force_login(test_user)
 
-        url = reverse("v1:quantum-functions-list")
+        url = reverse("v1:programs-list")
         with open(
             os.path.join(
                 os.path.dirname(os.path.realpath(__file__)),
@@ -116,68 +115,68 @@ class QuantumFunctionTests(APITestCase):
             ),
             "rb",
         ) as file:
-            quantum_function_input["artifact"] = file
+            program_input["artifact"] = file
             response = self.client.post(
-                url, data=quantum_function_input, format="multipart"
+                url, data=program_input, format="multipart"
             )
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Program.objects.count(), 2)
 
-    def test_count_of_all_quantum_functions_created_must_be_one(self):
+    def test_count_of_all_programs_created_must_be_one(self):
         """
-        List all the quantum functions created and check that there is one
+        List all the programs created and check that there is one
         """
-        url = reverse("v1:quantum-functions-list")
+        url = reverse("v1:programs-list")
         response = self.client.get(url, format="json")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.json()["count"], 1)
 
-    def test_delete_quantum_function_unauthenticated_returns_403(self):
+    def test_delete_program_unauthenticated_returns_403(self):
         """
-        Delete a quantum function with an unauthenticated user
+        Delete a program with an unauthenticated user
         """
-        quantum_function_id = "1a7947f9-6ae8-4e3d-ac1e-e7d608deec82"
+        program_id = "1a7947f9-6ae8-4e3d-ac1e-e7d608deec82"
 
-        url = reverse("v1:quantum-functions-detail", args=[quantum_function_id])
+        url = reverse("v1:programs-detail", args=[program_id])
         response = self.client.delete(url, format="json")
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
-    def test_delete_a_non_created_quantum_function_returns_404(self):
+    def test_delete_a_non_created_program_returns_404(self):
         """
-        Delete a quantum function that doesn't exist returns a 404
+        Delete a program that doesn't exist returns a 404
         """
         test_user = User.objects.get(username="test_user")
 
         self.client.force_login(test_user)
 
-        url = reverse("v1:quantum-functions-detail", args=[2])
+        url = reverse("v1:programs-detail", args=[2])
         response = self.client.delete(url, format="json")
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
-    def test_delete_quantum_function_returns_204(self):
+    def test_delete_program_returns_204(self):
         """
-        Delete a quantum function that exists 204
+        Delete a program that exists 204
         """
-        quantum_function_id = "1a7947f9-6ae8-4e3d-ac1e-e7d608deec82"
+        program_id = "1a7947f9-6ae8-4e3d-ac1e-e7d608deec82"
         test_user = User.objects.get(username="test_user")
 
         self.client.force_login(test_user)
 
-        url = reverse("v1:quantum-functions-detail", args=[quantum_function_id])
+        url = reverse("v1:programs-detail", args=[program_id])
         response = self.client.delete(url, format="json")
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(Program.objects.count(), 0)
 
-    def test_quantum_function_list_validation_returns_400(self):
+    def test_program_list_validation_returns_400(self):
         """
         The value for dependencies and tags is a dict, a non-allowed value for these fields and returns a 400
         """
         fields_to_check = ["dependencies", "tags"]
-        quantum_function_input = {
-            "title": "Awesome quantum function",
-            "description": "Awesome quantum function description",
-            "entrypoint": "quantum_function.py",
+        program_input = {
+            "title": "Awesome program",
+            "description": "Awesome program description",
+            "entrypoint": "program.py",
             "working_dir": "./",
             "version": "0.0.1",
             "dependencies": json.dumps({}),
@@ -190,7 +189,7 @@ class QuantumFunctionTests(APITestCase):
 
         self.client.force_login(test_user)
 
-        url = reverse("v1:quantum-functions-list")
+        url = reverse("v1:programs-list")
         with open(
             os.path.join(
                 os.path.dirname(os.path.realpath(__file__)),
@@ -200,24 +199,24 @@ class QuantumFunctionTests(APITestCase):
             ),
             "rb",
         ) as file:
-            quantum_function_input["artifact"] = file
+            program_input["artifact"] = file
             response = self.client.post(
-                url, data=quantum_function_input, format="multipart"
+                url, data=program_input, format="multipart"
             )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
         failed_validation_fields_list = list(response.json().keys())
         self.assertListEqual(failed_validation_fields_list, fields_to_check)
 
-    def test_quantum_function_dict_validation_returns_400(self):
+    def test_program_dict_validation_returns_400(self):
         """
         The value for env_vars and arguments is a list, a non-allowed value for these fields and returns a 400
         """
         fields_to_check = ["env_vars", "arguments"]
-        quantum_function_input = {
-            "title": "Awesome quantum function",
-            "description": "Awesome quantum function description",
-            "entrypoint": "quantum_function.py",
+        program_input = {
+            "title": "Awesome program",
+            "description": "Awesome program description",
+            "entrypoint": "program.py",
             "working_dir": "./",
             "version": "0.0.1",
             "dependencies": json.dumps([]),
@@ -230,7 +229,7 @@ class QuantumFunctionTests(APITestCase):
 
         self.client.force_login(test_user)
 
-        url = reverse("v1:quantum-functions-list")
+        url = reverse("v1:programs-list")
         with open(
             os.path.join(
                 os.path.dirname(os.path.realpath(__file__)),
@@ -240,24 +239,24 @@ class QuantumFunctionTests(APITestCase):
             ),
             "rb",
         ) as file:
-            quantum_function_input["artifact"] = file
+            program_input["artifact"] = file
             response = self.client.post(
-                url, data=quantum_function_input, format="multipart"
+                url, data=program_input, format="multipart"
             )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
         failed_validation_fields_list = list(response.json().keys())
         self.assertListEqual(failed_validation_fields_list, fields_to_check)
 
-    def test_quantum_function_artifact_validation_returns_400(self):
+    def test_program_artifact_validation_returns_400(self):
         """
         The value for artifact is not a tar file, a non-allowed value for this field and returns a 400
         """
         fields_to_check = ["artifact"]
-        quantum_function_input = {
-            "title": "Awesome quantum function",
-            "description": "Awesome quantum function description",
-            "entrypoint": "quantum_function.py",
+        program_input = {
+            "title": "Awesome program",
+            "description": "Awesome program description",
+            "entrypoint": "program.py",
             "working_dir": "./",
             "version": "0.0.1",
             "env_vars": json.dumps({"DEBUG": True}),
@@ -270,7 +269,7 @@ class QuantumFunctionTests(APITestCase):
 
         self.client.force_login(test_user)
 
-        url = reverse("v1:quantum-functions-list")
+        url = reverse("v1:programs-list")
 
         with open(
             os.path.join(
@@ -280,9 +279,9 @@ class QuantumFunctionTests(APITestCase):
                 "initial_data.json",
             )
         ) as file:
-            quantum_function_input["artifact"] = File(file)
+            program_input["artifact"] = File(file)
             response = self.client.post(
-                url, data=quantum_function_input, format="multipart"
+                url, data=program_input, format="multipart"
             )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
