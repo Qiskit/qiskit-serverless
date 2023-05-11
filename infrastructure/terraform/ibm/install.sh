1a85d375bf02bebb8929a6dd013801043da39515
david <9059044+Tansito@users.noreply.github.com>
2023-05-11 15:33:08 +0200

    Installation script to run terraform

---
+++ infrastructure/terraform/ibm/install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# We check for the API Token needed to log-in in IBM Cloud
+if [ -z "$IBMCLOUD_API_TOKEN" ]
+then
+  read -p "Introduce your IBM Cloud API Token: " IBMCLOUD_API_TOKEN
+fi
+
+# IBM Cloud CLI check
+if ! command -v ibmcloud &> /dev/null
+then
+    echo "IBM Cloud CLI not found. Please install it to continue."
+    exit 1
+fi
+
+# Terraform check
+if ! command -v ibmcloud &> /dev/null
+then
+    echo "IBM Cloud CLI not found. Please install it to continue."
+    exit 1
+fi
+
+# Log-in in IBM Cloud with API Token
+ibmcloud login --apikey "$IBMCLOUD_API_TOKEN"
+if [ "$?" -ne 0 ]
+then
+  echo "There was an error trying to log-in in IBM Cloud. Please review your API Token."
+fi
+
+# Execute terraform
+terraform apply -auto-approve
+if [ "$?" -ne 0 ]
+then
+  echo "There was a problem creating your cluster."
+fi
+
+# We do log-in in the cluster
+CLUSTER_ID=$(terraform output -raw cluster_id)
+
+ibmcloud ks cluster config --cluster "$CLUSTER_ID"
+
+kubectl config current-context
