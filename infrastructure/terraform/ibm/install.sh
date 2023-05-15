31c2e7c1688e1dda28abd72707177f94764a9bbe
david <9059044+Tansito@users.noreply.github.com>
2023-05-15 10:29:46 +0200

    Improved styles in terraform installation script

--- infrastructure/terraform/ibm/install.sh
+++ infrastructure/terraform/ibm/install.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
+# Introduction
+echo "This script will guide you to create a Kubernetes Cluster in IBM Cloud"
+
 # We check for the API Token needed to log-in in IBM Cloud
 if [ -z "$IBMCLOUD_API_TOKEN" ]
 then
-  read -p "Introduce your IBM Cloud API Token: " IBMCLOUD_API_TOKEN
+  read -p "
+  Introduce your IBM Cloud API Token: " IBMCLOUD_API_TOKEN
 fi
 
 # IBM Cloud CLI check
 if ! command -v ibmcloud &> /dev/null
 then
-    echo "IBM Cloud CLI not found. Please install it to continue."
+    echo "
+    IBM Cloud CLI not found. Please install it to continue."
     exit 1
 fi
 
 # Terraform check
 if ! command -v ibmcloud &> /dev/null
 then
-    echo "IBM Cloud CLI not found. Please install it to continue."
+    echo "
+    IBM Cloud CLI not found. Please install it to continue."
     exit 1
 fi
 
@@ -24,14 +30,16 @@ fi
 ibmcloud login --apikey "$IBMCLOUD_API_TOKEN"
 if [ "$?" -ne 0 ]
 then
-  echo "There was an error trying to log-in in IBM Cloud. Please review your API Token."
+  echo "
+  There was an error trying to log-in in IBM Cloud. Please review your API Token."
 fi
 
 # Execute terraform
 terraform apply -auto-approve
 if [ "$?" -ne 0 ]
 then
-  echo "There was a problem creating your cluster."
+  echo "
+  There was a problem creating your cluster."
 fi
 
 # We do log-in in the cluster
