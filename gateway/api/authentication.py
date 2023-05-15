bdf80554c1af86b598ee93b7b471b0b992869e9f
Iskandar Sitdikov <iskandar.sitdikov@gmail.com>
2023-05-15 11:58:58 -0400

    Gateway: custom auth, token vefirication

--- gateway/api/authentication.py
+++ gateway/api/authentication.py
@@ -3,6 +3,7 @@
 import json
 import logging
 from dataclasses import dataclass
+from typing import Callable, Any, Dict, Optional
 
 import requests
 from django.conf import settings
@@ -19,11 +20,30 @@ class CustomToken:
     token: str
 
 
+def safe_request(request: Callable) -> Optional[Dict[str, Any]]:
+    """Makes safe request and parses json response."""
+    result = None
+    response = None
+    try:
+        response = request()
+    except Exception as request_exception:  # pylint: disable=broad-exception-caught
+        logging.error(request_exception)
+
+    if response is not None and response.ok:
+        try:
+            result = json.loads(response.text)
+        except Exception as json_exception:  # pylint: disable=broad-exception-caught
+            logging.error(json_exception)
+
+    return result
+
+
 class CustomTokenBackend(authentication.BaseAuthentication):
     """Custom token backend for authentication against 3rd party auth service."""
 
     def authenticate(self, request):
         auth_url = settings.SETTINGS_TOKEN_AUTH_URL
+        verification_url = settings.SETTINGS_TOKEN_AUTH_VERIFICATION_URL
         auth_header = request.META.get("HTTP_AUTHORIZATION")
 
         user = None
@@ -31,26 +51,34 @@ class CustomTokenBackend(authentication.BaseAuthentication):
         if auth_header is not None and auth_url is not None:
             token = auth_header.split(" ")[-1]
 
-            response = requests.post(
-                auth_url,
-                json={settings.SETTINGS_TOKEN_AUTH_TOKEN_FIELD: token},
-                timeout=60,
+            auth_data = safe_request(
+                request=lambda: requests.post(
+                    auth_url,
+                    json={settings.SETTINGS_TOKEN_AUTH_TOKEN_FIELD: token},
+                    timeout=60,
+                )
             )
-            if response.ok:
-                try:
-                    json_response = json.loads(response.text)
-                    user_id = json_response.get(settings.SETTINGS_TOKEN_AUTH_USER_FIELD)
-                except ValueError as exception:
-                    logging.error(
-                        "Encountered exception on parsing response from %s", exception
-                    )
-                    user_id = None
+            if auth_data is not None:
+                user_id = auth_data.get(settings.SETTINGS_TOKEN_AUTH_USER_FIELD)
 
-                if user_id is not None:
-                    try:
-                        user = User.objects.get(username=user_id)
-                    except User.DoesNotExist:
-                        user = User(username=user_id)
-                        user.save()
+                verification_data = safe_request(
+                    request=lambda: requests.get(
+                        verification_url,
+                        headers={"Authorization": auth_data.get("id")},
+                        timeout=60,
+                    )
+                )
+
+                if verification_data is not None:
+                    verified = verification_data.get(
+                        settings.SETTINGS_TOKEN_AUTH_VERIFICATION_FIELD, False
+                    )
+
+                    if user_id is not None and verified:
+                        try:
+                            user = User.objects.get(username=user_id)
+                        except User.DoesNotExist:
+                            user = User(username=user_id)
+                            user.save()
 
         return user, CustomToken(token.encode()) if token else None
