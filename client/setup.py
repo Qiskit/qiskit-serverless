0f12c05839432674ea14ff266b3eec8c4d8a9ff5
Paul Schweigert <paul@paulschweigert.com>
2023-05-17 11:52:13 -0400

    Revert "Add explicit support for python 3.11 (#554)" (#560)
    This reverts commit 8eaa1f665bcee1326808542dd5003bec0f880442.
    
    Signed-off-by: Paul S. Schweigert <paul@paulschweigert.com>
    
    Ray has not released a Python 3.11 image yet, so need to remove
    them from the build process for the time being.

--- client/setup.py
+++ client/setup.py
@@ -38,7 +38,6 @@ setuptools.setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
         "Topic :: Scientific/Engineering :: Physics",
     ],
 )
