dfd15bb5ac8a2860c38d1f2ac01666d51b80d639
Iskandar Sitdikov <iskandar.sitdikov@gmail.com>
2023-05-30 16:31:09 -0400

    Issue 254 | Probes

--- gateway/probes/views.py
+++ gateway/probes/views.py
@@ -4,15 +4,7 @@ import requests
 
 
 def readiness(request):
-    try:
-        resp = requests.get(os.environ.get("RAY_HOST", "http://ray") + "/api/jobs/")
-    except Exception as err:
-        print(err)
-        return HttpResponse(status=503)
-    if 200 <= resp.status_code and resp.status_code < 500:
-        return HttpResponse("Hi! I'm ready.")
-    else:
-        return HttpResponse(status=503)
+    return HttpResponse("Hi! I'm ready.")
 
 
 def liveness(request):
