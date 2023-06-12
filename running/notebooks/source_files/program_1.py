64050e13dcb5d1339cf9aed29654188b7b5bf921
caleb-johnson <caleb-johnson@users.noreply.github.com>
2023-06-12 17:27:34 +0000

    deploy: 1968e1bba2571da3b833f535baf040d518c4be12

--- running/notebooks/source_files/program_1.py
+++ running/notebooks/source_files/program_1.py
@@ -3,6 +3,7 @@ from qiskit.primitives import Sampler
 
 from quantum_serverless import save_result
 
+print("Running program 1...")
 circuit = QuantumCircuit(2)
 circuit.h(0)
 circuit.cx(0, 1)
@@ -14,3 +15,4 @@ sampler = Sampler()
 quasi_dists = sampler.run(circuit).result().quasi_dists
 
 save_result(quasi_dists)
+print("Completed running program 1.")
