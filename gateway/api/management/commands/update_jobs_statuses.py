1ebe9f46b1ea2e3ea63fca448d4e99adff0de193
Akihiko (Aki) Kuroda <16141898+akihikokuroda@users.noreply.github.com>
2023-06-21 18:26:15 -0400

    enhance error handling (#694)
    Signed-off-by: Akihiko Kuroda <akihikokuroda2020@gmail.com>

--- gateway/api/management/commands/update_jobs_statuses.py
+++ gateway/api/management/commands/update_jobs_statuses.py
@@ -4,6 +4,7 @@ from ray.dashboard.modules.job.sdk import JobSubmissionClient
 
 from api.models import Job
 from api.utils import ray_job_status_to_model_job_status
+from api.ray import kill_ray_cluster
 
 
 class Command(BaseCommand):
@@ -16,13 +17,21 @@ class Command(BaseCommand):
         updated_jobs_counter = 0
         for job in Job.objects.filter(status__in=Job.RUNNING_STATES):
             if job.compute_resource:
-                ray_client = JobSubmissionClient(job.compute_resource.host)
-                # update job logs
-                job.logs = ray_client.get_job_logs(job.ray_job_id)
-                # update job status
-                ray_job_status = ray_job_status_to_model_job_status(
-                    ray_client.get_job_status(job.ray_job_id)
-                )
+                try:
+                    ray_client = JobSubmissionClient(job.compute_resource.host)
+                    # update job logs
+                    job.logs = ray_client.get_job_logs(job.ray_job_id)
+                    # update job status
+                    ray_job_status = ray_job_status_to_model_job_status(
+                        ray_client.get_job_status(job.ray_job_id)
+                    )
+                except (ConnectionError, RuntimeError):
+                    kill_ray_cluster(job.compute_resource.title)
+                    job.compute_resource.delete()
+                    job.compute_resource = None
+                    ray_job_status = Job.FAILED
+                    job.logs = "Something went wrong during compute resource instance."
+
                 if ray_job_status != job.status:
                     self.stdout.write(
                         f"Job [{job.id}] status changed from [{job.status}] to [{ray_job_status}]"
