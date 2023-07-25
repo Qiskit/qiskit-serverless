a1dc0c6acf64cc22f9a434d50d98670165c4aa84
Iskandar Sitdikov <IceKhan13@users.noreply.github.com>
2023-07-25 16:15:11 -0400

    Issue 741 | Docs: remove deprecated algorithms module (#816)

--- docs/development/examples/source_files/vqe/vqe.py
+++ docs/development/examples/source_files/vqe/vqe.py
@@ -1,34 +1,113 @@
 import logging
 from typing import Optional
-
+import time
 import numpy as np
+from scipy.optimize import minimize
 
 from qiskit import QuantumCircuit
 from qiskit.primitives import BaseEstimator, Estimator as QiskitEstimator
-from qiskit.algorithms.optimizers import SPSA, Optimizer
 from qiskit.opflow import PauliSumOp
-from qiskit.algorithms.minimum_eigensolvers import VQE
 
 from qiskit_ibm_runtime import QiskitRuntimeService, Estimator, Session, Options
 
 from quantum_serverless import QuantumServerless, distribute_task, get_arguments, get, save_result
 
 
+def build_callback(ansatz, hamiltonian, estimator, callback_dict):
+    """Return callback function that uses Estimator instance,
+    and stores intermediate values into a dictionary.
+
+    Parameters:
+        ansatz (QuantumCircuit): Parameterized ansatz circuit
+        hamiltonian (SparsePauliOp): Operator representation of Hamiltonian
+        estimator (Estimator): Estimator primitive instance
+        callback_dict (dict): Mutable dict for storing values
+
+    Returns:
+        Callable: Callback function object
+    """
+
+    def callback(current_vector):
+        """Callback function storing previous solution vector,
+        computing the intermediate cost value, and displaying number
+        of completed iterations and average time per iteration.
+
+        Values are stored in pre-defined 'callback_dict' dictionary.
+
+        Parameters:
+            current_vector (ndarray): Current vector of parameters
+                                      returned by optimizer
+        """
+        # Keep track of the number of iterations
+        callback_dict["iters"] += 1
+        # Set the prev_vector to the latest one
+        callback_dict["prev_vector"] = current_vector
+        # Compute the value of the cost function at the current vector
+        callback_dict["cost_history"].append(
+            estimator.run(ansatz, hamiltonian, parameter_values=current_vector).result().values[0]
+        )
+        # Grab the current time
+        current_time = time.perf_counter()
+        # Find the total time of the execute (after the 1st iteration)
+        if callback_dict["iters"] > 1:
+            callback_dict["_total_time"] += current_time - callback_dict["_prev_time"]
+        # Set the previous time to the current time
+        callback_dict["_prev_time"] = current_time
+        # Compute the average time per iteration and round it
+        time_str = (
+            round(callback_dict["_total_time"] / (callback_dict["iters"] - 1), 2)
+            if callback_dict["_total_time"]
+            else "-"
+        )
+        # Print to screen on single line
+        print(
+            "Iters. done: {} [Avg. time per iter: {}]".format(callback_dict["iters"], time_str),
+            end="\r",
+            flush=True,
+        )
+
+    return callback
+
+
+def cost_func(params, ansatz, hamiltonian, estimator):
+    """Return estimate of energy from estimator
+
+    Parameters:
+        params (ndarray): Array of ansatz parameters
+        ansatz (QuantumCircuit): Parameterized ansatz circuit
+        hamiltonian (SparsePauliOp): Operator representation of Hamiltonian
+        estimator (Estimator): Estimator primitive instance
+
+    Returns:
+        float: Energy estimate
+    """
+    energy = estimator.run(ansatz, hamiltonian, parameter_values=params).result().values[0]
+    return energy
+
+
 def run_vqe(
-    estimator: BaseEstimator,
-    ansatz: QuantumCircuit,
-    operator: PauliSumOp,
-    optimizer: Optimizer,
-    initial_parameters: Optional[np.ndarray] = None
+    initial_parameters,
+    ansatz,
+    operator,
+    estimator,
+    method
 ):
-    vqe = VQE(
-        estimator,
-        ansatz,
-        optimizer,
-        initial_point=initial_parameters,
-        callback=lambda idx, parameters, mean, std: print(f"{idx}: {mean}")
+    callback_dict = {
+        "prev_vector": None,
+        "iters": 0,
+        "cost_history": [],
+        "_total_time": 0,
+        "_prev_time": None,
+    }
+    callback = build_callback(ansatz, operator, estimator, callback_dict)
+    result = minimize(
+        cost_func,
+        initial_parameters,
+        args=(ansatz, operator, estimator),
+        method=method,
+        callback=callback,
     )
-    return vqe.compute_minimum_eigenvalue(operator)
+    return result, callback_dict
 
 
 if __name__ == '__main__':
@@ -38,20 +117,12 @@ if __name__ == '__main__':
     
     ansatz = arguments.get("ansatz")
     operator = arguments.get("operator")
+    method = arguments.get("method", "COBYLA")
     initial_parameters = arguments.get("initial_parameters")
-    
-    optimizers = {
-        "spsa": SPSA()
-    }
-    optimizer = optimizers.get(arguments.get("optimizer", "nan"))
-    if optimizer is None:
-        raise Exception(
-            f"Optimizer {optimizer} is not is a list of available optimizers [{list(optimizers.keys())}]"
-        )
-
-    aux_operators = arguments.get("aux_operators")
-    if aux_operators is not None:
-        logging.warning("`aux_operators` are not supported yet.")
+    if initial_parameters is None:
+        initial_parameters = 2 * np.pi * np.random.rand(ansatz.num_parameters)
+        
+    print(initial_parameters)
         
     if service is not None:
         # if we have service we need to open a session and create estimator
@@ -62,33 +133,22 @@ if __name__ == '__main__':
             options.optimization_level = 3
 
             estimator = Estimator(session=session, options=options)
-            vqe_result = run_vqe(
-                estimator=estimator,
-                ansatz=ansatz,
-                operator=operator,
-                optimizer=optimizer,
-                initial_parameters=initial_parameters
-            )
     else:
         # if we do not have a service let's use standart local estimator
         estimator = QiskitEstimator()
-        vqe_result = run_vqe(
-            estimator=estimator,
-            ansatz=ansatz,
-            operator=operator,
-            optimizer=optimizer,
-            initial_parameters=initial_parameters
-        )
+
+    vqe_result, callback_dict = run_vqe(
+        initial_parameters=initial_parameters,
+        ansatz=ansatz,
+        operator=operator,
+        estimator=estimator,
+        method=method
+    )
 
     save_result({
-        "aux_operator_eigenvalues": [],
-        "cost_function_evals": vqe_result.cost_function_evals,
-        "eigenstate": None,
-        "eigenvalue": vqe_result.eigenvalue,
-        "optimal_parameters": None,
-        "optimal_point": vqe_result.optimal_point.tolist(),
-        "optimal_value": vqe_result.optimal_value,
-        "optimizer_evals": vqe_result.optimizer_evals,
-        "optimizer_history": {},
-        "optimizer_time": vqe_result.optimizer_time
+        "optimal_point": vqe_result.x.tolist(),
+        "optimal_value": vqe_result.fun,
+        "optimizer_evals": vqe_result.nfev,
+        "optimizer_history": callback_dict.get("cost_history", []),
+        "optimizer_time": callback_dict.get("_total_time", 0)
     })
