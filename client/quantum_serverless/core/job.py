847c31488120355b15479e3df67c8195947ec4ae
Caleb Johnson <calebj1524@outlook.com>
2023-11-06 10:06:55 -0600

    Print unknown statuses rather than error

--- client/quantum_serverless/core/job.py
+++ client/quantum_serverless/core/job.py
@@ -739,4 +739,4 @@ def _map_status_to_serverless(status: str) -> str:
     try:
         return status_map[status]
     except KeyError as exc:
-        raise KeyError(f"Cannot interpret unknown job status: {status}") from exc
+        print(f"{status}")
