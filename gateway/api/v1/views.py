4aacccb289a84184ca592e6072a01558b88de3b8
David <9059044+Tansito@users.noreply.github.com>
2023-11-20 16:10:47 -0500

    removed self from static methods

--- gateway/api/v1/views.py
+++ gateway/api/v1/views.py
@@ -22,11 +22,11 @@ class ProgramViewSet(views.ProgramViewSet):  # pylint: disable=too-many-ancestor
     permission_classes = [permissions.IsAuthenticated]
 
     @staticmethod
-    def get_serializer_job_class(self):
+    def get_serializer_job_class():
         return v1_serializers.JobSerializer
 
     @staticmethod
-    def get_service_program_class(self):
+    def get_service_program_class():
         return v1_services.ProgramService
 
     def get_serializer_class(self):
