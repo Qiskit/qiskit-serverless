7e375b89e0a384dbb8fb15a96226e2824066845e
David <9059044+Tansito@users.noreply.github.com>
2023-11-20 15:17:52 -0500

    created base program service class

---
+++ gateway/api/services.py
@@ -0,0 +1,57 @@
+"""
+Services for api application:
+    - Program Service
+    - Job Service
+
+Version services inherit from the different services.
+"""
+
+import logging
+
+from .models import Program
+from .exceptions import InternalServerErrorException
+
+logger = logging.getLogger("services")
+
+class ProgramService():
+    
+    @staticmethod
+    def save(serializer, request) -> Program:
+        
+        title = serializer.data.get("title")
+        author = request.user
+
+        existing_program = (
+            Program.objects.filter(title=title, author=author)
+            .order_by("-created")
+            .first()
+        )
+
+        if existing_program is not None:
+            program = existing_program
+            program.arguments = serializer.data.get("arguments")
+            program.entrypoint = serializer.data.get("entrypoint")
+            program.dependencies = serializer.data.get("dependencies", "[]")
+            program.env_vars = serializer.data.get("env_vars", "{}")
+        else:
+            program = Program(**serializer.data)
+        program.artifact = request.FILES.get("artifact")
+        program.author = author
+
+        ## TODO: It would be nice if we could unify all the saves logic in one unique entry-point
+        try:
+            program.save()
+        except (
+            Exception
+        ) as save_program_exception:
+            logger.error(
+                "Exception was caught saving the program [%s] by [%s] \n"
+                "Error trace: %s",
+                title,
+                author,
+                save_program_exception
+            )
+            raise InternalServerErrorException("Unexpected error saving the program")
+        
+        return program
+    
