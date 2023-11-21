8a5573b87dd4c3c87ac7d826993bd2be541fe32e
David <davidgv@ibm.com>
2023-11-21 10:51:38 -0500

    push selector image to the private repository

--- ci/internal-makefile.mk
+++ ci/internal-makefile.mk
@@ -12,9 +12,11 @@ DOCKER_FILE_GATEWAY       ?= ./gateway/Dockerfile
 DOCKER_FILE_NOTEBOOK      ?= ./Dockerfile-notebook
 DOCKER_FILE_RAY_NODE      ?= ./Dockerfile-ray-node
 DOCKER_FILE_REPOSITORY    ?= ./repository/Dockerfile
+DOCKER_FILE_SELECTOR  	  ?= ./tools/Dockerfile
 
-DOCKER_REGISTRY           := icr.io
-DOCKER_REGISTRY_NAMESPACE := quantum-public
+DOCKER_REGISTRY           			:= icr.io
+DOCKER_REGISTRY_NAMESPACE 			:= quantum-public
+DOCKER_PRIVATE_REGISTRY_NAMESPACE 	:= quantum-experimental
 
 TARGET_SERVICE			  ?= quantum-serverless
 ENVIRONMENT               ?= development
@@ -24,6 +26,8 @@ DOCKER_IMAGE_GATEWAY      := $(DOCKER_REGISTRY)/$(DOCKER_REGISTRY_NAMESPACE)/qua
 DOCKER_IMAGE_NOTEBOOK     := $(DOCKER_REGISTRY)/$(DOCKER_REGISTRY_NAMESPACE)/quantum-serverless-notebook
 DOCKER_IMAGE_RAY_NODE     := $(DOCKER_REGISTRY)/$(DOCKER_REGISTRY_NAMESPACE)/quantum-serverless-ray-node
 DOCKER_IMAGE_REPOSITORY   := $(DOCKER_REGISTRY)/$(DOCKER_REGISTRY_NAMESPACE)/quantum-repository-server
+DOCKER_IMAGE_SELECTOR 	  := $(DOCKER_REGISTRY)/$(DOCKER_PRIVATE_REGISTRY_NAMESPACE)/quantum-serverless-selector
+DOCKER_IMAGE_SELECTOR_TAG := 0.8.0-test
 
 # =========
 # CI Commands
@@ -45,6 +49,10 @@ docker/lint-ray: docker/lint
 docker/lint-repository: DOCKER_FILE 		:= $(DOCKER_FILE_REPOSITORY) 
 docker/lint-repository: docker/lint
 
+# .PHONY: docker/lint-selector
+# docker/lint-selector: DOCKER_FILE 		:= $(DOCKER_FILE_SELECTOR) 
+# docker/lint-selector: docker/lint
+
 .PHONY: docker/sast-gateway
 docker/sast-gateway: DOCKER_FILE 			:= $(DOCKER_FILE_GATEWAY) 
 docker/sast-gateway: docker/sast
@@ -60,6 +68,10 @@ docker/sast-ray: docker/sast
 .PHONY: docker/sast-repository
 docker/sast-repository: DOCKER_FILE 		:= $(DOCKER_FILE_REPOSITORY) 
 docker/sast-repository: docker/sast
+
+# .PHONY: docker/sast-selector
+# docker/sast-selector: DOCKER_FILE 		:= $(DOCKER_FILE_SELECTOR) 
+# docker/sast-selector: docker/sast
 
 .PHONY: docker/vscan-gateway
 docker/vscan-gateway: DOCKER_FILE 			:= $(DOCKER_FILE_GATEWAY)
@@ -116,3 +128,17 @@ docker/vscan-repository: DOCKER_IMAGE 		:= $(DOCKER_IMAGE_REPOSITORY)
 docker/vscan-repository: IMAGE_TAG 			:= $(PROJECT_VERSION)
 docker/vscan-repository: PY_VERSION 		:= "3.9"
 docker/vscan-repository: docker/vscan
+
+# .PHONY: docker/vscan-selector
+# docker/vscan-selector: DOCKER_FILE 			:= $(DOCKER_FILE_SELECTOR)
+# docker/vscan-selector: DOCKER_IMAGE 		:= $(DOCKER_IMAGE_SELECTOR)
+# docker/vscan-selector: IMAGE_TAG 			:= $(PROJECT_VERSION)
+# docker/vscan-selector: PY_VERSION 			:= "3.10"
+# docker/vscan-selector: docker/vscan
+
+.PHONY: docker/release-selector
+docker/release-selector: DOCKER_FILE 		:= $(DOCKER_FILE_SELECTOR)
+docker/release-selector: DOCKER_IMAGE 		:= $(DOCKER_IMAGE_SELECTOR)
+docker/release-selector: IMAGE_TAG 			:= $(DOCKER_IMAGE_SELECTOR_TAG)
+docker/release-selector: PY_VERSION 		:= "3.10"
+docker/release-selector: docker/release
