4f2ee3175828ff05b915562c19d724ee0877c7d4
Akihiko Kuroda <akihikokuroda2020@gmail.com>
2024-02-27 17:45:35 -0500

    create thread for each direction

--- gateway/proxy/proxy/server.py
+++ gateway/proxy/proxy/server.py
@@ -8,47 +8,65 @@ HOST = "127.0.0.1"
 PORT = 8443
 
 def process_connection(connection):
+    print("Recieving data")
+    data = connection.recv(1024)
+    if not data:
+        print("no data")
+        return
+        print("data received from client")
+    print(f"Received: {data}")
+    parser = headerparser.HeaderParser()
+    parser.add_field('Host', required=True)
+    parser.add_field('Accept', required=False)
+    parser.add_field('User-Agent', required=False)
+    parser.add_field('Content-length', required=False)
+    parser.add_field('Content-type', required=False)
+    parser.add_field('Accept-Encoding')
+    parser.add_field('Connection')
+    parser.add_field('X-Qx-Client-Application')
+    parser.add_field('X-Access-Token')
+    _, headers = data.decode('utf-8').split('\r\n', 1)
+    msg = parser.parse(headers)
+    host = msg["Host"].split(":", -1)
+    print(host[0])
+
+    c_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+    c_context.load_verify_locations(certifi.where())
+    client_s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client = c_context.wrap_socket(client_s, server_hostname=host[0])
+
+    print("connecting backend")
+    client.connect((host[0], 443))
+    print("connected")
+    client.sendall(data)
+    print("data sent to backend")
+    t = threading.Thread(target=process_from_backend, args=(client, connection, ))
+    t.start()
+    t = threading.Thread(target=process_from_program, args=(connection, client, ))
+    t.start()
+    print("piping threads created")
+
+def process_from_backend(client, connection):
     while True:
-        print("Recieving data")
+        data = client.recv(4096)
+        if not data:
+            print("no data")
+            break
+        print("data received from backend")
+        print(f"Received: {data}")
+        connection.send(data)
+    print("receive from backend completed")
+
+def process_from_program(connection, client):
+    while True:
         data = connection.recv(1024)
         if not data:
             print("no data")
             break
-        print(f"Received: {data.decode('utf-8')}")
-        print(data)
-        parser = headerparser.HeaderParser()
-        parser.add_field('Host', required=True)
-        parser.add_field('Accept', required=False)
-        parser.add_field('User-Agent', required=False)
-        parser.add_field('Content-length', required=False)
-        parser.add_field('Content-type', required=False)
-        parser.add_field('Accept-Encoding')
-        parser.add_field('Connection')
-        parser.add_field('X-Qx-Client-Application')
-        parser.add_field('X-Access-Token')
-        _, headers = data.decode('utf-8').split('\r\n', 1)
-        msg = parser.parse(headers)
-        host = msg["Host"].split(":", -1)
-        print(host[0])
-
-        c_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-        c_context.load_verify_locations(certifi.where())
-        client_s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client = c_context.wrap_socket(client_s, server_hostname=host[0])
-
-        print("connecting backend")
-        client.connect((host[0], 443))
-        print("eonnected")
+        print("data received from program")
+        print(f"Received: {data}")
         client.sendall(data)
-        print("data sent to backend")
-        data = client.recv(4096)
-        print("data received from backend")
-
-        connection.send(data)
-        print(f"Received: {data.decode('utf-8')}")
-        break
-    print("receive completed")
-
+    print("receive from program completed")
 
 if __name__ == "__main__":
     context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
