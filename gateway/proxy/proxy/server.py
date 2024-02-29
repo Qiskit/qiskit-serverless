9c0b7d01de4fb7cca773a56ec9134da68b583cce
Akihiko Kuroda <akihikokuroda2020@gmail.com>
2024-02-28 21:27:11 -0500

    data logs

--- gateway/proxy/proxy/server.py
+++ gateway/proxy/proxy/server.py
@@ -3,6 +3,7 @@ import ssl
 import headerparser
 import threading
 import certifi
+import gzip
 
 HOST = "127.0.0.1"
 PORT = 8443
@@ -17,14 +18,7 @@ def process_connection(connection):
     print(f"Received: {data}")
     parser = headerparser.HeaderParser()
     parser.add_field('Host', required=True)
-    parser.add_field('Accept', required=False)
-    parser.add_field('User-Agent', required=False)
-    parser.add_field('Content-length', required=False)
-    parser.add_field('Content-type', required=False)
-    parser.add_field('Accept-Encoding')
-    parser.add_field('Connection')
-    parser.add_field('X-Qx-Client-Application')
-    parser.add_field('X-Access-Token')
+    parser.add_additional()
     _, headers = data.decode('utf-8').split('\r\n', 1)
     msg = parser.parse(headers)
     host = msg["Host"].split(":", -1)
@@ -47,14 +41,41 @@ def process_connection(connection):
     print("piping threads created")
 
 def process_from_backend(client, connection):
+    parser = headerparser.HeaderParser()
+    parser.add_field('Content-Type', required=False)
+    parser.add_field('Transfer-Encoding', required=False)
+    parser.add_field('Content-Encoding', required=False)
+    parser.add_field('Connection', required=False)
+    parser.add_additional()
+    alldata = b''
+    in_header = True
+    
     while True:
         data = client.recv(4096)
         if not data:
             print("no data")
             break
         print("data received from backend")
-        print(f"Received: {data}")
+        #print(f"Received: {data}")
         connection.send(data)
+        alldata = alldata+data
+        if in_header :
+            _, headers = alldata.split(b'\x0d\x0a', 1)
+            if headers.find(b'\x0d\x0a\x0d\x0a') != -1:
+                headers, _ = headers.split(b'\x0d\x0a\x0d\x0a', 1)
+            if headers:
+                msg = parser.parse(headers.decode('utf-8'))
+                print(f"Received header from backend: {headers.decode('utf-8')}")
+                in_header = False
+                if not "Content-Encoding" in msg or not msg["Content-Encoding"] == "gzip":
+                    print(f"Received: {alldata}")
+        if data.find(b'0\r\n\r\n') != -1:
+            print("end of chunked data received from backend")
+            # print(alldata)
+            if "Content-Encoding" in msg and msg["Content-Encoding"] == "gzip":
+                print(gzip.decompress(alldata.split(b'\r\n\r\n',2)[1].split(b'\r\n',2)[1]))
+            if not "Connection" in msg or not msg["Connection"] == "keep-active":
+                break
     print("receive from backend completed")
 
 def process_from_program(connection, client):
@@ -88,4 +109,3 @@ if __name__ == "__main__":
         print("new connection")
         t = threading.Thread(target=process_connection, args=(connection, ))
         t.start()
-
