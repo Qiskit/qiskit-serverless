2b66f0f2faeed223a565df07926a748c62b6ce09
Akihiko Kuroda <akihikokuroda2020@gmail.com>
2024-04-02 17:08:32 -0400

    non-root user

--- proxy/proxy/entrypoint.sh
+++ proxy/proxy/entrypoint.sh
@@ -2,16 +2,15 @@
 
 echo "entrypoint.sh"
 
-iptables -t nat -N proxy_redirect
-iptables -t nat -A proxy_redirect -p tcp -d 104.18.29.94 -j REDIRECT --to-ports 8443
-iptables -t nat -A proxy_redirect -p tcp -d 104.18.28.94 -j REDIRECT --to-ports 8443
+sudo iptables -t nat -N proxy_redirect
+sudo iptables -t nat -A proxy_redirect -p tcp -d 104.18.29.94 -j REDIRECT --to-ports 8443
+sudo iptables -t nat -A proxy_redirect -p tcp -d 104.18.28.94 -j REDIRECT --to-ports 8443
 
-iptables -t nat -N proxy_output
-iptables -t nat -A proxy_output -p tcp --dport 6379 -j RETURN
-iptables -t nat -A proxy_output -p tcp -m owner --gid-owner 123 -j RETURN
-iptables -t nat -A proxy_output -j proxy_redirect
+sudo iptables -t nat -N proxy_output
+sudo iptables -t nat -A proxy_output -p tcp --dport 6379 -j RETURN
+sudo iptables -t nat -A proxy_output -p tcp -m owner --gid-owner 123 -j RETURN
+sudo iptables -t nat -A proxy_output -j proxy_redirect
 
- iptables -t nat -A OUTPUT -p tcp -j proxy_output
+sudo iptables -t nat -A OUTPUT -p tcp -j proxy_output
 
 python httpserver.py
-
