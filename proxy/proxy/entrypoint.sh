5f3348523e421b85b39718cb935a91493f128b76
Akihiko Kuroda <akihikokuroda2020@gmail.com>
2024-04-03 13:08:22 -0400

    debug kubernetes test

--- proxy/proxy/entrypoint.sh
+++ proxy/proxy/entrypoint.sh
@@ -2,15 +2,15 @@
 
 echo "entrypoint.sh"
 
-#sudo iptables -t nat -N proxy_redirect
-#sudo iptables -t nat -A proxy_redirect -p tcp -d 104.18.29.94 -j REDIRECT --to-ports 8443
-#sudo iptables -t nat -A proxy_redirect -p tcp -d 104.18.28.94 -j REDIRECT --to-ports 8443
+sudo iptables -t nat -N proxy_redirect
+sudo iptables -t nat -A proxy_redirect -p tcp -d 104.18.29.94 -j REDIRECT --to-ports 8443
+sudo iptables -t nat -A proxy_redirect -p tcp -d 104.18.28.94 -j REDIRECT --to-ports 8443
 
-#sudo iptables -t nat -N proxy_output
-#sudo iptables -t nat -A proxy_output -p tcp --dport 6379 -j RETURN
-#sudo iptables -t nat -A proxy_output -p tcp -m owner --gid-owner 123 -j RETURN
-#sudo iptables -t nat -A proxy_output -j proxy_redirect
+sudo iptables -t nat -N proxy_output
+sudo iptables -t nat -A proxy_output -p tcp --dport 6379 -j RETURN
+sudo iptables -t nat -A proxy_output -p tcp -m owner --gid-owner 123 -j RETURN
+sudo iptables -t nat -A proxy_output -j proxy_redirect
 
-#sudo iptables -t nat -A OUTPUT -p tcp -j proxy_output
+sudo iptables -t nat -A OUTPUT -p tcp -j proxy_output
 
 python httpserver.py
