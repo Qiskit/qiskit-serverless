674a79c9350452c8bc97fef38866a5c8f933dc3b
David <9059044+Tansito@users.noreply.github.com>
2024-04-08 14:11:08 -0400

    Update migrations scripts

--- gateway/api/migrations/0017_alter_program_title.py
+++ gateway/api/migrations/0017_alter_program_title.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("api", "0014_runtimejob"),
+        ("api", "0016_catalogentry_status"),
     ]
 
     operations = [
