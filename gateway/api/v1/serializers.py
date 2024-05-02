0398cc1718926bddd27e9106137814ecfc1c7183
Iskandar Sitdikov <iskandar.sitdikov@gmail.com>
2024-05-02 11:34:26 -0400

    linter

--- gateway/api/v1/serializers.py
+++ gateway/api/v1/serializers.py
@@ -29,13 +29,15 @@ class UploadProgramSerializer(serializers.UploadProgramSerializer):
         """Validaes image."""
         # place to add image validation
         return value
-    
+
     def validate(self, attrs):
         """Validates serializer data."""
         entrypoint = attrs.get("entrypoint", None)
         image = attrs.get("image", None)
         if entrypoint is None and image is None:
-            raise ValidationError("At least one of attributes (entrypoint, image) is required.")
+            raise ValidationError(
+                "At least one of attributes (entrypoint, image) is required."
+            )
         return super().validate(attrs)
 
     class Meta(serializers.UploadProgramSerializer.Meta):
