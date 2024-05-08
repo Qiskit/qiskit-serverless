d9e0dabf85f012b18cbe6f5095b1cfd991a802d7
David <9059044+Tansito@users.noreply.github.com>
2024-05-08 17:59:08 -0400

    Removed tests to review errors

--- tests/experimental/file_download.py
+++ tests/experimental/file_download.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
 import os
-from qiskit_serverless import ServerlessProvider, QiskitFunction
+from qiskit_serverless import ServerlessClient, QiskitFunction
 
-serverless = ServerlessProvider(
+serverless = ServerlessClient(
     token=os.environ.get("GATEWAY_TOKEN", "awesome_token"),
     host=os.environ.get("GATEWAY_HOST", "http://localhost:8000"),
 )
