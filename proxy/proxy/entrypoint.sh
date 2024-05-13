7ce2136321f45aa19b3e3a494c3ef07e149e3583
Akihiko (Aki) Kuroda <16141898+akihikokuroda@users.noreply.github.com>
2024-05-13 16:07:13 -0400

    Put correct proxy (#1330)
    * restore missing commits

--- proxy/proxy/entrypoint.sh
+++ proxy/proxy/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "entrypoint.sh"
-python httpserver.py
+gunicorn -w 4 'wsgiproxy:app' -b "127.0.0.1:8443" --keyfile /etc/ray/tls/tls.key  --certfile /etc/ray/tls/tls.crt  --ca-certs /etc/ca/tls/ca.crt  --access-logfile=-
