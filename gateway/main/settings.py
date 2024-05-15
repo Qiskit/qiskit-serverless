479ec1a2c18e457de207820a6e80b2dc698577cf
Akihiko Kuroda <akihikokuroda2020@gmail.com>
2024-05-15 12:35:45 -0400

    unlock

--- gateway/main/settings.py
+++ gateway/main/settings.py
@@ -58,7 +58,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework.authtoken",
     "rest_framework_simplejwt",
-    "allauth"
+    "allauth",
     "allauth.socialaccount",
     "api",
     "psycopg2",
