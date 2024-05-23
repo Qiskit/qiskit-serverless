92cf627a5a931cbe6b541f35354d1a8ec60ff617
David <9059044+Tansito@users.noreply.github.com>
2024-05-23 11:52:04 -0400

    Updated documentation

--- tests/basic/source_files/pattern_with_dependencies.py
+++ tests/basic/source_files/pattern_with_dependencies.py
@@ -2,26 +2,7 @@
 
 from qiskit_serverless import save_result
 
-# from qiskit.primitives import Sampler
-# from qiskit_experiments.library import StandardRB
 import pendulum
-
-
-# arguments = get_arguments()
-
-# circuit = arguments.get("circuit")
-
-# rb = StandardRB(physical_qubits=(1,), lengths=list(range(1, 300, 30)), seed=42)
-# composed = circuit.compose(rb.circuits()[0])
-
-# sampler = Sampler()
-
-# quasi_dists = sampler.run(composed).result().quasi_dists
-
-# print(f"Quasi distribution: {quasi_dists[0]}")
-
-# # saving results of a program
-# save_result({"quasi_dists": quasi_dists[0]})
 
 dt_toronto = pendulum.datetime(2012, 1, 1, tz='America/Toronto')
 dt_vancouver = pendulum.datetime(2012, 1, 1, tz='America/Vancouver')
