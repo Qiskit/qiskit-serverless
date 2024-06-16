0d0b454946c2bcd049296b68a2869eca1ef16788
Akihiko Kuroda <akihikokuroda2020@gmail.com>
2024-06-16 13:20:49 -0400

    image build test

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,15 +8,15 @@ version=latest
 repository=icr.io/quantum-public
 arch="amd64"
 
-rayNodeImageName=$(repository)/qiskit-serverless-ray-node
-gatewayImageName=$(repository)/qiskit-serverless-gateway
-proxyImageName=$(repository)/qiskit-serverless-proxy
+rayNodeImageName=$repository/qiskit-serverless-ray-node
+gatewayImageName=$repository/qiskit-serverless-gateway
+proxyImageName=$repository/qiskit-serverless-proxy
 
-docker build -t $(rayNodeImageName):$(version) --build-arg TARGETARCH=$(arch) -f Dockerfile-ray-node .
-docker build -t $(rayNodeImageName):$(version)-py310 --build-arg TARGETARCH=$(arch) --build-arg IMAGE_PY_VERSION=py310 -f Dockerfile-ray-node .
-docker build -t $(rayNodeImageName):$(version)-py39  --build-arg TARGETARCH=$(arch) --build-arg IMAGE_PY_VERSION=py39  -f Dockerfile-ray-node .
-docker build -t $(gatewayImageName):$(version) -f ./gateway/Dockerfile .
-docker build -t $(proxyImageName):$(version) -f ./proxy/Dockerfile .
+docker build -t $rayNodeImageName:$version --build-arg TARGETARCH=$arch -f Dockerfile-ray-node .
+docker build -t $rayNodeImageName:$version-py310 --build-arg TARGETARCH=$arch --build-arg IMAGE_PY_VERSION=py310 -f Dockerfile-ray-node .
+docker build -t $rayNodeImageName:$version-py39  --build-arg TARGETARCH=$arch --build-arg IMAGE_PY_VERSION=py39  -f Dockerfile-ray-node .
+docker build -t $gatewayImageName:$version -f ./gateway/Dockerfile .
+docker build -t $proxyImageName:$version -f ./proxy/Dockerfile .
 
 docker images
 echo "end"
