919089824596fbcc0d0e9a18dc31b01acd40d04c
Akihiko Kuroda <akihikokuroda2020@gmail.com>
2024-06-17 16:26:56 -0400

    setup tox

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,8 +4,14 @@ SCRIPT_NAME=$(basename $0)
 echo "Running ${SCRIPT_NAME}"
 
 cd gateway
+
+yum install -y python3.11
+yum install -y python3-pip
+ln -s /usr/bin/python3.11 /usr/bin/python
+pip3 install --upgrade pip
+ln /usr/bin/pip3 /usr/bin/pip
 pip install tox
 pip install -r requirements.txt --no-cache-dir
 
 tox -elint
-tox -e py310
+tox -epy310
