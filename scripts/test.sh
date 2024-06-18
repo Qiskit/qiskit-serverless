cc3c280921d5aad467ba9c3ea68257c7f005c4ef
Akihiko Kuroda <akihikokuroda2020@gmail.com>
2024-06-18 17:44:15 -0400

    test disable BOM generation

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,15 +3,60 @@
 SCRIPT_NAME=$(basename $0)
 echo "Running ${SCRIPT_NAME}"
 
-cd gateway
+#!/usr/bin/env bash
 
-#yum install -y python3.10
-#yum install -y python3-pip
-#ln -s /usr/bin/python3.10 /usr/bin/python
-#pip3 install --upgrade pip
-#ln /usr/bin/pip3 /usr/bin/pip
-#pip install tox
-#pip install -r requirements.txt --no-cache-dir
+# shellcheck disable=SC2230
+if ! which python3; then
+  echo "Installing Python 3..."
+  yum update -yq && yum install -yq python3-pip
+fi
 
-tox -elint
-tox -epy310
+echo ""
+python3 --version
+echo ""
+
+# shellcheck disable=SC2230
+if ! which pip3; then
+  echo "Installing pip3..."
+  yum update -yq && yum install -yq python3-pip
+fi
+
+echo ""
+pip3 -V
+echo ""
+
+echo "Installing required modules..."
+pip3 install flask
+pip3 install prometheus_client
+pip3 install flasgger
+pip3 install ibmcloudenv
+echo ""
+
+# set -x
+echo "Executing unit-tests..."
+python3 -W ignore::ResourceWarning -m unittest discover -s tests -v -p "*.py" > "$WORKSPACE/unit-tests.log" 2>&1
+
+exit_code=$?
+status="success"
+if [ $exit_code == "0" ]; then
+  echo "Unit tests completed OK..."
+else
+  echo "Error executing unit tests !!!"
+  status="failure"
+fi
+echo ""
+cat "$WORKSPACE/unit-tests.log"
+echo ""
+
+collect-evidence \
+    --tool-type "unittest" \
+    --status "$status" \
+    --evidence-type "com.ibm.unit_tests" \
+    --asset-type "repo" \
+    --asset-key "app-repo" \
+    --attachment "$WORKSPACE/unit-tests.log"
+
+echo "Saving results..."
+save_result test "$WORKSPACE/unit-tests.log"
+
+exit $exit_code
