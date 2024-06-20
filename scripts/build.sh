b951e34c65d6793fd62ade7e99bfa2a96a3798d2
Akihiko Kuroda <akihikokuroda2020@gmail.com>
2024-06-20 14:21:22 -0400

    test build artifact

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,8 +3,12 @@
 set -euo pipefail
 
 # shellcheck disable=SC2086
-docker build $DOCKER_BUILD_ARGS .
-docker push "${IMAGE}"
+docker build $GATEWAY_DOCKER_BUILD_ARGS .
+docker build $PROXY_DOCKER_BUILD_ARGS .
+docker build $RAY_NODE_DOCKER_BUILD_ARGS .
+docker push "${GATEWAY_IMAGE}"
+docker push "${PROXY_IMAGE}"
+docker push "${RAY_NODE_IMAGE}"
 
 DIGEST="$(docker inspect --format='{{index .RepoDigests 0}}' "${IMAGE}" | awk -F@ '{print $2}')"
 
@@ -12,45 +16,61 @@ DIGEST="$(docker inspect --format='{{index .RepoDigests 0}}' "${IMAGE}" | awk -F
 # Save the artifact to the pipeline, 
 # so it can be scanned and signed later
 #
-save_artifact app-image \
+save_artifact gateway-image \
     type=image \
-    "name=${IMAGE}" \
+    "name=${GATEWAY_IMAGE}" \
     "digest=${DIGEST}" \
     "tags=${IMAGE_TAG}"
 
-#
-# Make sure you connect the built artifact to the repo and commit
-# it was built from. The source repo asset format is:
-#   <repo_URL>.git#<commit_SHA>
-#
-# In this example we have a repo saved as `app-repo`,
-# and we've used the latest cloned state to build the image.
-#
 url="$(load_repo app-repo url)"
 sha="$(load_repo app-repo commit)"
 
-save_artifact app-image \
+save_artifact gateway-image \
+"source=${url}.git#${sha}"
+
+save_artifact proxy-image \
+    type=image \
+    "name=${PROXY_IMAGE}" \
+    "digest=${DIGEST}" \
+    "tags=${IMAGE_TAG}"
+
+url="$(load_repo app-repo url)"
+sha="$(load_repo app-repo commit)"
+
+save_artifact proxy-image \
+"source=${url}.git#${sha}"
+
+save_artifact ray-node-image \
+    type=image \
+    "name=${RAY_NODE_IMAGE}" \
+    "digest=${DIGEST}" \
+    "tags=${IMAGE_TAG}"
+
+url="$(load_repo app-repo url)"
+sha="$(load_repo app-repo commit)"
+
+save_artifact ray_node-image \
 "source=${url}.git#${sha}"
 
 # optional tags
-set +e
-TAG="$(cat /config/custom-image-tag)"
-set -e
-if [[ "${TAG}" ]]; then
-    #see build_setup script
-    IFS=',' read -ra tags <<< "${TAG}"
-    for i in "${!tags[@]}"
-    do
-        TEMP_TAG=${tags[i]}
-        TEMP_TAG=$(echo "$TEMP_TAG" | sed -e 's/^[[:space:]]*//')
-        echo "adding tag $i $TEMP_TAG"
-        ADDITIONAL_IMAGE_TAG="$ICR_REGISTRY_DOMAIN/$ICR_REGISTRY_NAMESPACE/$IMAGE_NAME:$TEMP_TAG"
-        docker tag "$IMAGE" "$ADDITIONAL_IMAGE_TAG"
-        docker push "$ADDITIONAL_IMAGE_TAG"
-
-        # save tags to pipelinectl
-        tags="$(load_artifact app-image tags)"
-        save_artifact app-image "tags=${tags},${TEMP_TAG}"
-    done
-fi
+#set +e
+#TAG="$(cat /config/custom-image-tag)"
+#set -e
+#if [[ "${TAG}" ]]; then
+#    #see build_setup script
+#    IFS=',' read -ra tags <<< "${TAG}"
+#    for i in "${!tags[@]}"
+#    do
+#        TEMP_TAG=${tags[i]}
+#        TEMP_TAG=$(echo "$TEMP_TAG" | sed -e 's/^[[:space:]]*//')
+#        echo "adding tag $i $TEMP_TAG"
+#        ADDITIONAL_IMAGE_TAG="$ICR_REGISTRY_DOMAIN/$ICR_REGISTRY_NAMESPACE/$IMAGE_NAME:$TEMP_TAG"
+#        docker tag "$IMAGE" "$ADDITIONAL_IMAGE_TAG"
+#        docker push "$ADDITIONAL_IMAGE_TAG"
+#
+#        # save tags to pipelinectl
+#        tags="$(load_artifact app-image tags)"
+#        save_artifact app-image "tags=${tags},${TEMP_TAG}"
+#    done
+#fi
 
--- scripts/build_setup.sh
+++ scripts/build_setup.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+`a`\`/usr/bin/env bash
 
 set -euo pipefail
 
@@ -66,45 +66,46 @@ else
   REPOSITORY="$(load_repo app-repo url)"
 fi
 
-IMAGE_NAME="$(get_env image-name "$(basename "$REPOSITORY" .git)")"
+#IMAGE_NAME="$(get_env image-name "$(basename "$REPOSITORY" .git)")"
+GATEWAY_IMAGE_NAME="$(get_env image-name "$(basename "$REPOSITORY" .git)")-gateway"
+PROXY_IMAGE_NAME="$(get_env image-name "$(basename "$REPOSITORY" .git)")-proxy"
+RAY_NODE__IMAGE_NAME="$(get_env image-name "$(basename "$REPOSITORY" .git)")-ray_node"
 IMAGE_TAG="$(date +%Y%m%d%H%M%S)-$(cat /config/git-branch | tr -c '[:alnum:]_.-' '_')-$(cat /config/git-commit)"
 IMAGE_TAG=${IMAGE_TAG////_}
 
-if [[ -f "/config/break_glass" ]]; then
-  ARTIFACTORY_URL="$(jq -r .parameters.repository_url /config/artifactory)"
-  ARTIFACTORY_REGISTRY="$(sed -E 's~https://(.*)/?~\1~' <<<"$ARTIFACTORY_URL")"
-  ARTIFACTORY_INTEGRATION_ID="$(jq -r .instance_id /config/artifactory)"
-  IMAGE="$ARTIFACTORY_REGISTRY/$IMAGE_NAME:$IMAGE_TAG"
-  jq -j --arg instance_id "$ARTIFACTORY_INTEGRATION_ID" '.services[] | select(.instance_id == $instance_id) | .parameters.token' /toolchain/toolchain.json | docker login -u "$(jq -r '.parameters.user_id' /config/artifactory)" --password-stdin "$(jq -r '.parameters.repository_url' /config/artifactory)"
+# ICR_REGISTRY_NAMESPACE="$(cat /config/registry-namespace)"
+ICR_REGISTRY_NAMESPACE="quantum-public"
+ICR_REGISTRY_DOMAIN="$(get_env registry-domain "")"
+if [ -z "$ICR_REGISTRY_DOMAIN" ]; then
+  # Default to icr domain from registry-region
+  ICR_REGISTRY_REGION="$(get-icr-region "$(cat /config/registry-region)")"
+  ICR_REGISTRY_DOMAIN="$ICR_REGISTRY_REGION.icr.io"
+fi
+GATEWY_IMAGE="$ICR_REGISTRY_DOMAIN/$ICR_REGISTRY_NAMESPACE/$GATEWAY_IMAGE_NAME:$IMAGE_TAG"
+PROXY_IMAGE="$ICR_REGISTRY_DOMAIN/$ICR_REGISTRY_NAMESPACE/$PROXY_IMAGE_NAME:$IMAGE_TAG"
+RAY_NODE_IMAGE="$ICR_REGISTRY_DOMAIN/$ICR_REGISTRY_NAMESPACE/$RAY_NODEIMAGE_NAME:$IMAGE_TAG"
+docker login -u iamapikey --password-stdin "$ICR_REGISTRY_DOMAIN" < /config/api-key
+
+# Create the namespace if needed to ensure the push will be can be successfull
+echo "Checking registry namespace: ${ICR_REGISTRY_NAMESPACE}"
+IBM_LOGIN_REGISTRY_REGION=$(< /config/registry-region awk -F: '{print $3}')
+ibmcloud config --check-version false
+ibmcloud login --apikey @/config/api-key -r "$IBM_LOGIN_REGISTRY_REGION" -a "$IBMCLOUD_API"
+NS=$( ibmcloud cr namespaces | sed 's/ *$//' | grep -x "${ICR_REGISTRY_NAMESPACE}" ||: )
+
+if [ -z "${NS}" ]; then
+    echo "Registry namespace ${ICR_REGISTRY_NAMESPACE} not found"
+    ibmcloud cr namespace-add "${ICR_REGISTRY_NAMESPACE}"
+    echo "Registry namespace ${ICR_REGISTRY_NAMESPACE} created."
 else
-  # ICR_REGISTRY_NAMESPACE="$(cat /config/registry-namespace)"
-  ICR_REGISTRY_NAMESPACE="quantum-public"
-  ICR_REGISTRY_DOMAIN="$(get_env registry-domain "")"
-  if [ -z "$ICR_REGISTRY_DOMAIN" ]; then
-    # Default to icr domain from registry-region
-    ICR_REGISTRY_REGION="$(get-icr-region "$(cat /config/registry-region)")"
-    ICR_REGISTRY_DOMAIN="$ICR_REGISTRY_REGION.icr.io"
-  fi
-  IMAGE="$ICR_REGISTRY_DOMAIN/$ICR_REGISTRY_NAMESPACE/$IMAGE_NAME:$IMAGE_TAG"
-  docker login -u iamapikey --password-stdin "$ICR_REGISTRY_DOMAIN" < /config/api-key
-
-  # Create the namespace if needed to ensure the push will be can be successfull
-  echo "Checking registry namespace: ${ICR_REGISTRY_NAMESPACE}"
-  IBM_LOGIN_REGISTRY_REGION=$(< /config/registry-region awk -F: '{print $3}')
-  ibmcloud config --check-version false
-  ibmcloud login --apikey @/config/api-key -r "$IBM_LOGIN_REGISTRY_REGION" -a "$IBMCLOUD_API"
-  NS=$( ibmcloud cr namespaces | sed 's/ *$//' | grep -x "${ICR_REGISTRY_NAMESPACE}" ||: )
-
-  if [ -z "${NS}" ]; then
-      echo "Registry namespace ${ICR_REGISTRY_NAMESPACE} not found"
-      ibmcloud cr namespace-add "${ICR_REGISTRY_NAMESPACE}"
-      echo "Registry namespace ${ICR_REGISTRY_NAMESPACE} created."
-  else
-      echo "Registry namespace ${ICR_REGISTRY_NAMESPACE} found."
-  fi
+    echo "Registry namespace ${ICR_REGISTRY_NAMESPACE} found."
 fi
 
 # shellcheck disable=SC2034 # next sourced script is using it where this script is also sourced
-DOCKER_BUILD_ARGS="-t $IMAGE"
+GATEWAY_DOCKER_BUILD_ARGS="-t $GATEWAY_IMAGE"
+PROXY_DOCKER_BUILD_ARGS="-t $PROXY_IMAGE"
+RAY_NODE_DOCKER_BUILD_ARGS="-t $RAY_NODE_IMAGE"
 echo "end of build_setup"
-echo $IMAGE
+echo $GATEWAY_IMAGE
+echo $PROXY_IMAGE
+echo $RAY_NODE_IMAGE
