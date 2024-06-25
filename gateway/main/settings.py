11c30b1fc89c15818b585610ad1e4674769a83ce
Paul S. Schweigert <paul@paulschweigert.com>
2024-06-25 12:13:45 -0400

    update log message with limits
    Signed-off-by: Paul S. Schweigert <paul@paulschweigert.com>

--- gateway/main/settings.py
+++ gateway/main/settings.py
@@ -306,6 +306,8 @@ SETTINGS_TOKEN_AUTH_VERIFICATION_FIELD = os.environ.get(
 # resources limitations
 LIMITS_JOBS_PER_USER = int(os.environ.get("LIMITS_JOBS_PER_USER", "2"))
 LIMITS_MAX_CLUSTERS = int(os.environ.get("LIMITS_MAX_CLUSTERS", "6"))
+LIMITS_CPU_PER_TASK = int(os.environ.get("LIMITS_CPU_PER_TASK", "4"))
+LIMITS_MEMORY_PER_TASK = int(os.environ.get("LIMITS_MEMORY_PER_TASK", "8"))
 
 # ray cluster management
 RAY_KUBERAY_NAMESPACE = os.environ.get("RAY_KUBERAY_NAMESPACE", "qiskit-serverless")
