4c10747a2830faff77946cb9af01f0d1e13c216a
David <9059044+Tansito@users.noreply.github.com>
2024-07-17 16:34:41 -0400

    Fix tasks initialization

--- gateway/api/apps.py
+++ gateway/api/apps.py
@@ -4,6 +4,20 @@ from django.apps import AppConfig
 from django.db.models.signals import post_migrate
 
 
+def on_migrations_applied(sender, **kwargs):  # pylint: disable=unused-argument
+    """
+    This method will run tasks to populate the DB after the migration
+    """
+    # This import may be here for the correct initialization of the App
+    from api.tasks import (  # pylint: disable=import-outside-toplevel
+        programs,
+        providers,
+    )
+
+    providers.assign_admin_group()
+    programs.assign_run_permission()
+
+
 class ApiConfig(AppConfig):
     """ApiConfig."""
 
@@ -11,14 +25,4 @@ class ApiConfig(AppConfig):
     name = "api"
 
     def ready(self):
-        def on_migrations_applied(**kwargs):
-            # This import may be here for the correct initialization of the App
-            from api.tasks import (  # pylint: disable=import-outside-toplevel
-                programs,
-                providers,
-            )
-
-            providers.assign_admin_group()
-            programs.assign_run_permission()
-
-        post_migrate.connect(on_migrations_applied)
+        post_migrate.connect(on_migrations_applied, sender=self)
