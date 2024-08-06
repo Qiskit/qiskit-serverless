ae2477f378ddb58e0ff5b497cff53a2d107f1e56
Paul S. Schweigert <paul@paulschweigert.com>
2024-08-06 15:12:01 -0400

    option to allowlist all versions of dependency
    Signed-off-by: Paul S. Schweigert <paul@paulschweigert.com>

--- gateway/api/v1/serializers.py
+++ gateway/api/v1/serializers.py
@@ -63,7 +63,7 @@ class UploadProgramSerializer(serializers.UploadProgramSerializer):
                     raise ValidationError(f"Dependency {dep} is not allowed")
 
                 # Determine if a specific version of a dependency is allowed
-                if ver not in allowlist[dep]:
+                if allowlist[dep] and ver not in allowlist[dep]:
                     raise ValidationError(f"Version {ver} of dependency {dep} is not allowed")
 
 
