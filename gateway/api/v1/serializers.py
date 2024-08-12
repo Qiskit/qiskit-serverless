932e9f95e6e88af610602d57d8a8a5d6b1aa5b05
Paul Schweigert <paul@paulschweigert.com>
2024-08-12 11:18:59 -0400

    Implement allowlist framework for dependencies (#1443)
    * implement allowlist framework for dependencies
    
    Signed-off-by: Paul S. Schweigert <paul@paulschweigert.com>
    
    Allow operators to specify an allowlist of dependencies and allowed
    versions.
    
    The allowlist is stored in a config file.
    
    A sample allowlist might look like:
    
        allowlist = { "wheel": ["0.44.0", "0.43.2"] }
    
    which would imply that the wheel package is allowed, but only versions
    0.43.2 and 0.44.0 .
    
    For this PR, the allowlist is empty, which means all dependencies are
    allowed, and it is stored locally. In a future PR, this should move
    into a Kubernetes configmap so that it can be updated without having
    to rebuild the container.
    
    Each dependency can optionally specify a list of allowed versions. If
    the list is empty, then all versions of the dependency are allowed.
    
    An example:
    
        allowlist = { "wheel": [] }
    
    This could also be adapted to store a minimum allowed version instead
    of listing all allowed versions, depending on requirements.
    
    * option to allowlist all versions of dependency
    
    Signed-off-by: Paul S. Schweigert <paul@paulschweigert.com>
    
    * lint
    
    Signed-off-by: Paul S. Schweigert <paul@paulschweigert.com>
    
    * specify encoding
    
    Signed-off-by: Paul S. Schweigert <paul@paulschweigert.com>
    
    * use envvar for allowlist config
    
    Signed-off-by: Paul S. Schweigert <paul@paulschweigert.com>
    
    * lint
    
    Signed-off-by: Paul S. Schweigert <paul@paulschweigert.com>
    
    * add tests for gateway allowlist functionality
    
    Signed-off-by: Paul S. Schweigert <paul@paulschweigert.com>
    
    * lint
    
    Signed-off-by: Paul S. Schweigert <paul@paulschweigert.com>
    
    * review comments
    
    Signed-off-by: Paul S. Schweigert <paul@paulschweigert.com>
    
    * lint
    
    Signed-off-by: Paul S. Schweigert <paul@paulschweigert.com>
    
    * catch and log errors opening/decoding allowlist
    
    Signed-off-by: Paul S. Schweigert <paul@paulschweigert.com>
    
    * lint
    
    Signed-off-by: Paul S. Schweigert <paul@paulschweigert.com>
    
    * lint again
    
    Signed-off-by: Paul S. Schweigert <paul@paulschweigert.com>
    
    * lint 3
    
    Signed-off-by: Paul S. Schweigert <paul@paulschweigert.com>
    
    ---------
    
    Signed-off-by: Paul S. Schweigert <paul@paulschweigert.com>

--- gateway/api/v1/serializers.py
+++ gateway/api/v1/serializers.py
@@ -2,9 +2,14 @@
 Serializers api for V1.
 """
 
+import json
+import logging
 from rest_framework.serializers import ValidationError
+from django.conf import settings
 from api import serializers
 from api.models import Provider
+
+logger = logging.getLogger("gateway.serializers")
 
 
 class ProgramSerializer(serializers.ProgramSerializer):
@@ -34,7 +39,7 @@ class UploadProgramSerializer(serializers.UploadProgramSerializer):
         # place to add image validation
         return value
 
-    def validate(self, attrs):
+    def validate(self, attrs):  # pylint: disable=too-many-branches
         """Validates serializer data."""
         entrypoint = attrs.get("entrypoint", None)
         image = attrs.get("image", None)
@@ -42,6 +47,39 @@ class UploadProgramSerializer(serializers.UploadProgramSerializer):
             raise ValidationError(
                 "At least one of attributes (entrypoint, image) is required."
             )
+
+        # validate dependencies
+        # allowlist stored in json config file (eventually via configmap)
+        # sample:
+        # allowlist = { "wheel": ["0.44.0", "0.43.2"] }
+        # where the values for each key are allowed versions of dependency
+        deps = json.loads(attrs.get("dependencies", None))
+        try:
+            with open(
+                settings.GATEWAY_ALLOWLIST_CONFIG, encoding="utf-8", mode="r"
+            ) as f:
+                allowlist = json.load(f)
+        except IOError as e:
+            logger.error("Unable to open allowlist config file: %s", e)
+            raise ValueError("Unable to open allowlist config file") from e
+        except ValueError as e:
+            logger.error("Unable to decode dependency allowlist: %s", e)
+            raise ValueError("Unable to decode dependency allowlist") from e
+
+        # If no allowlist specified, all dependencies allowed
+        if len(allowlist.keys()) > 0:
+            for d in deps:
+                dep, ver = d.split("==")
+
+                # Determine if a dependency is allowed
+                if dep not in allowlist:
+                    raise ValidationError(f"Dependency {dep} is not allowed")
+
+                # Determine if a specific version of a dependency is allowed
+                if allowlist[dep] and ver not in allowlist[dep]:
+                    raise ValidationError(
+                        f"Version {ver} of dependency {dep} is not allowed"
+                    )
 
         title = attrs.get("title")
         provider = attrs.get("provider", None)
