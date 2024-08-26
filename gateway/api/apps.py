81cc293a80620ee50c9fabce9c372d0179b7872a
David <9059044+Tansito@users.noreply.github.com>
2024-08-26 13:37:24 -0400

    Remove tasks (#1468)
    * Remove tasks references from api
    
    * Remove settings from main
    
    * Remove tasks from helm

--- gateway/api/apps.py
+++ gateway/api/apps.py
@@ -1,21 +1,6 @@
 """Applications."""
 
 from django.apps import AppConfig
-from django.db.models.signals import post_migrate
-
-
-def on_migrations_applied(sender, **kwargs):  # pylint: disable=unused-argument
-    """
-    This method will run tasks to populate the DB after the migration
-    """
-    # This import may be here for the correct initialization of the App
-    from api.tasks import (  # pylint: disable=import-outside-toplevel
-        programs,
-        providers,
-    )
-
-    providers.assign_admin_groups()
-    programs.assign_run_permission()
 
 
 class ApiConfig(AppConfig):
@@ -23,6 +8,3 @@ class ApiConfig(AppConfig):
 
     default_auto_field = "django.db.models.BigAutoField"
     name = "api"
-
-    def ready(self):
-        post_migrate.connect(on_migrations_applied, sender=self)
