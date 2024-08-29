3704b575d211361e461bcff9ba7a8c0f59fda06e
Akihiko Kuroda <akihikokuroda2020@gmail.com>
2024-08-29 19:26:20 -0400

    lint

--- client/qiskit_serverless/core/function.py
+++ client/qiskit_serverless/core/function.py
@@ -127,7 +127,9 @@ class QiskitFunction:  # pylint: disable=too-many-instance-attributes
         Returns:
             Job ids : job executed this function
         """
-        from qiskit_serverless.core.job import Job  # pylint: disable=import-outside-toplevel
+        from qiskit_serverless.core.job import (   # pylint: disable=import-outside-toplevel
+            Job,
+        )
 
         if self.job_client is None:
             raise ValueError("No clients specified for a function.")
