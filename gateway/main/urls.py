5093b57299df6be2d073640501cb2b4f35704797
Gregorio Iniesta <korgan00@gmail.com>
2024-09-17 18:37:14 +0200

    delete unused import

--- gateway/main/urls.py
+++ gateway/main/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.static import serve
 from django.contrib import admin
 from django.urls import path, include, re_path
 from rest_framework import routers, permissions
