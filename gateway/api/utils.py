f18a5d5cc1ee64bfe1ac7a93d7093a52ff03dd4f
Paul S. Schweigert <paul@paulschweigert.com>
2024-09-25 19:30:16 -0400

    lint
    Signed-off-by: Paul S. Schweigert <paul@paulschweigert.com>

--- gateway/api/utils.py
+++ gateway/api/utils.py
@@ -129,7 +129,10 @@ def build_env_variables(token, job: Job, args: str = None) -> Dict[str, str]:
         if sys.getsizeof(args) < 1000000:
             arguments = args
         else:
-            logger.warn("arguments for job [%s] are > 1MB and will not be written to env var", job.id)
+            logger.warn(
+                "arguments for job [%s] are > 1MB and will not be written to env var",
+                job.id
+            )
 
     if settings.SETTINGS_AUTH_MECHANISM != "default":
         extra = {
