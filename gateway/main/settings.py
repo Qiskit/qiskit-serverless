250405b632d3105ba94afc80b95c46abc4c5adea
David <9059044+Tansito@users.noreply.github.com>
2024-09-27 12:48:20 -0400

    Restore the changes and remove socialaccount

--- gateway/main/settings.py
+++ gateway/main/settings.py
@@ -71,8 +71,8 @@ INSTALLED_APPS = [
     "django_prometheus",
     "rest_framework",
     "rest_framework.authtoken",
+    "rest_framework_simplejwt",
     "allauth",
-    "allauth.socialaccount",
     "api",
     "psycopg2",
     "drf_yasg",
@@ -231,7 +231,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # =============
 SETTINGS_AUTH_MECHANISM = os.environ.get("SETTINGS_AUTH_MECHANISM", "default")
 SETTINGS_DEFAULT_AUTH_CLASSES = [
-    # "dj_rest_auth.jwt_auth.JWTCookieAuthentication",
+    "rest_framework_simplejwt.authentication.JWTAuthentication",
+    "dj_rest_auth.jwt_auth.JWTCookieAuthentication",
 ]
 ALL_AUTH_CLASSES_CONFIGURATION = {
     "default": SETTINGS_DEFAULT_AUTH_CLASSES,
