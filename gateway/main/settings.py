8ba2a2a240ad1eb1a9621aecd82eefee6fbe9524
David <9059044+Tansito@users.noreply.github.com>
2024-09-27 12:21:47 -0400

    Remove simplejwt from auth classes

--- gateway/main/settings.py
+++ gateway/main/settings.py
@@ -231,7 +231,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # =============
 SETTINGS_AUTH_MECHANISM = os.environ.get("SETTINGS_AUTH_MECHANISM", "default")
 SETTINGS_DEFAULT_AUTH_CLASSES = [
-    # "rest_framework_simplejwt.authentication.JWTAuthentication",
     "dj_rest_auth.jwt_auth.JWTCookieAuthentication",
 ]
 ALL_AUTH_CLASSES_CONFIGURATION = {
