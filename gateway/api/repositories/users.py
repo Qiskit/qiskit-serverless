d1ae928de4ae1db2fdc23517e745de84f58a3eee
David <9059044+Tansito@users.noreply.github.com>
2025-01-17 13:49:30 -0500

    remove unneeded change

--- gateway/api/repositories/users.py
+++ gateway/api/repositories/users.py
@@ -2,39 +2,15 @@
 Repository implementation for Groups model
 """
 
-import logging
 from typing import List
-from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group, Permission
 from django.db.models import Q
-
-
-User = get_user_model()
-logger = logging.getLogger("gateway")
 
 
 class UserRepository:  # pylint: disable=too-few-public-methods
     """
     The main objective of this class is to manage the access to the model
     """
-
-    def get_user_by_id(self, user_id: str):
-        """
-        Returns a user by its id
-
-        Args:
-            user_id: id of the user in database
-
-        Returns:
-            User | None: if it exists
-        """
-
-        result_queryset = User.objects.filter(id=user_id).first()
-
-        if result_queryset is None:
-            logger.warning("Function [%s] was not found", user_id)
-
-        return result_queryset
 
     def get_groups_by_permissions(self, user, permission_name: str) -> List[Group]:
         """
