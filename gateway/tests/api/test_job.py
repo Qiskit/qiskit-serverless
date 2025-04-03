9e699c01f77c3abb122cecd96b6884df03a8c56e
Gregorio Iniesta <korgan00@gmail.com>
2025-04-03 13:49:59 +0200

    added tests for sub-status

--- gateway/tests/api/test_job.py
+++ gateway/tests/api/test_job.py
@@ -15,9 +15,9 @@ class TestJobApi(APITestCase):
 
     fixtures = ["tests/fixtures/fixtures.json"]
 
-    def _authorize(self):
+    def _authorize(self, username="test_user"):
         """Authorize client."""
-        user = models.User.objects.get(username="test_user")
+        user = models.User.objects.get(username=username)
         self.client.force_authenticate(user=user)
 
     def test_job_non_auth_user(self):
@@ -294,7 +294,7 @@ class TestJobApi(APITestCase):
         self.assertEqual(response_details.data.get("sub_status"), "MAPPING")
 
     def test_job_update_sub_status_wrong_value(self):
-        """Test job update sub status with wrong sub-status"""
+        """Test job update sub status with wrong sub-status value"""
         self._authorize()
 
         job_id = "8317718f-5c0d-4fb6-9947-72e480b85048"
@@ -307,8 +307,35 @@ class TestJobApi(APITestCase):
         self.assertEqual(response_sub_status.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response_sub_status.data.get("message"), "'sub_status' not provided or is not valid")
 
+    def test_job_update_sub_status_empty_value(self):
+        """Test job update sub status with empty sub-status"""
+        self._authorize()
+
+        job_id = "8317718f-5c0d-4fb6-9947-72e480b85048"
+        response_sub_status = self.client.post(
+            reverse("v1:jobs-sub-status", args=[job_id]),
+            format="json"
+        )
+
+        self.assertEqual(response_sub_status.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response_sub_status.data.get("message"), "'sub_status' not provided or is not valid")
+
+    def test_job_update_sub_status_wrong_user(self):
+        """Test job update sub status with unauthorized user"""
+        self._authorize(username="test_user_2")
+
+        job_id = "8317718f-5c0d-4fb6-9947-72e480b85048"
+        response_sub_status = self.client.post(
+            reverse("v1:jobs-sub-status", args=[job_id]),
+            format="json",
+            data={"sub_status": "MAPPING"},
+        )
+
+        self.assertEqual(response_sub_status.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(response_sub_status.data.get("message"), f"Job [{job_id}] not found")
+
     def test_job_update_sub_status_not_running(self):
-        """Test job update status"""
+        """Test job update sub status not in running state"""
         self._authorize()
 
         job_id = "57fc2e4d-267f-40c6-91a3-38153272e764"
