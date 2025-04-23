3ac887ea244f93f5ee443c57cb4722cb7fd94a00
Gregorio Iniesta <korgan00@gmail.com>
2025-04-23 10:21:09 +0200

    set correct headers

--- client/qiskit_serverless/core/job.py
+++ client/qiskit_serverless/core/job.py
@@ -306,6 +306,8 @@ def update_status(status: str):
         )
         return False
 
+    instance = os.environ.get(ENV_JOB_GATEWAY_INSTANCE, None)
+    
     url = (
         f"{os.environ.get(ENV_JOB_GATEWAY_HOST)}/"
         f"api/{version}/jobs/{os.environ.get(ENV_JOB_ID_GATEWAY)}/sub_status/"
@@ -313,7 +315,7 @@ def update_status(status: str):
     response = requests.post(
         url,
         data={"sub_status": status},
-        headers={"Authorization": f"Bearer {token}"},
+        headers=get_headers(token=token, instance=instance),
         timeout=REQUESTS_TIMEOUT,
     )
     if not response.ok:
