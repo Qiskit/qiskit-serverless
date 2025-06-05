7b6e9836a0860bc00c1af2644e7d26c7d6efcadc
David <9059044+Tansito@users.noreply.github.com>
2025-06-05 15:44:48 -0400

    fix an error generating the urls

--- gateway/main/urls.py
+++ gateway/main/urls.py
@@ -63,5 +63,5 @@ urlpatterns += [
     re_path(r"^redoc/$", schema.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
 if settings.DEBUG:
-    urlpatterns += path("", include("django_prometheus.urls"))
+    urlpatterns += [path("", include("django_prometheus.urls"))]
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
