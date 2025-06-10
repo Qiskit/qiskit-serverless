acb7150f9cd81049db5f1b000bd446beb15ef832
Pablo Aragón <pablo.aragon@ibm.com>
2025-06-10 13:36:06 +0200

    fix all url compositions in files client

--- client/qiskit_serverless/utils/urls.py
+++ client/qiskit_serverless/utils/urls.py
@@ -7,4 +7,4 @@ def url_path_join(base, *parts):
     """
     Join URL parts with single slashes.
     """
-    return "/".join([base.rstrip("/")] + [p.strip("/") for p in parts])
+    return "/".join([base.rstrip("/")] + [p for p in parts])
