035a3a835b14558897f1c5ad95a0cc2643681f5a
David <9059044+Tansito@users.noreply.github.com>
2025-06-17 11:11:22 -0400

    include additional tests

--- gateway/tests/api/utils/test_cluster_name_generator.py
+++ gateway/tests/api/utils/test_cluster_name_generator.py
@@ -13,11 +13,17 @@ class TestClusterNameGenerator(APITestCase):
         username = "6bc1230a4g12340012345003"
 
         cluster_name = generate_cluster_name(username=username)
-        assert cluster_name.startswith("c-6bc1230a4g12340012345003-")
+        self.assertTrue(cluster_name.startswith("c-6bc1230a4g12340012345003-"))
 
     def test_ibm_cloud_user_generation(self):
         """This test verifies the cluster name generation for IBM Cloud users."""
         username = "IBMid-123001ABCD"
 
         cluster_name = generate_cluster_name(username=username)
-        assert cluster_name.startswith("c-ibmid-123001abcd-")
+        self.assertTrue(cluster_name.startswith("c-ibmid-123001abcd-"))
+
+    def test_weird_user_generation(self):
+        """This test verifies the cluster name generation for a weird usern name."""
+        username = "WE.IRD_?|-name"
+        cluster_name = generate_cluster_name(username=username)
+        self.assertTrue(cluster_name.startswith("c-we-ird----name-"))
