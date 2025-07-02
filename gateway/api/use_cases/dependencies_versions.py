b05c69ccefc8b915e200271a27d56284600856d7
korgan00 <korgan00@hotmail.com>
2025-07-02 13:22:06 +0200

    fix return anotation

--- gateway/api/use_cases/dependencies_versions.py
+++ gateway/api/use_cases/dependencies_versions.py
@@ -1,8 +1,8 @@
 """Authentication use case to manage the authentication process in the api."""
 
 import logging
-from typing import Optional
-from django.contrib.auth.models import AbstractUser
+from typing import Dict
+from pkg_resources import Requirement
 
 from api.utils import create_dynamic_dependencies_whitelist
 
@@ -15,7 +15,7 @@ class AvailableDependenciesVersionsUseCase:  # pylint: disable=too-few-public-me
     This class will available dynamic dependencies.
     """
 
-    def execute(self) -> Optional[type[AbstractUser]]:
+    def execute(self) -> Dict[str, Requirement]:
         """
         Get the dependencies from the whitlist
         """
