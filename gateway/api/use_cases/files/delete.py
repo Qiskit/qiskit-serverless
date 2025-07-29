fc819878b31036100e22d1ccdb5ed596920b06cd
korgan00 <korgan00@hotmail.com>
2025-07-29 18:27:26 +0200

    added delete endpoints

---
+++ gateway/api/use_cases/files/delete.py
@@ -0,0 +1,52 @@
+"""Authentication use case to manage the authentication process in the api."""
+# pylint: disable=duplicate-code
+import logging
+from typing import List
+from api.services.file_storage import FileStorage, WorkingDir
+from api.repositories.functions import FunctionRepository
+from api.domain.exceptions.not_found_error import NotFoundError
+
+from api.models import RUN_PROGRAM_PERMISSION
+
+
+logger = logging.getLogger("gateway.use_cases.files")
+
+
+class FilesDeleteUseCase:
+    """
+    This class will return available dynamic dependencies on execute.
+    """
+
+    function_repository = FunctionRepository()
+    working_dir = WorkingDir.USER_STORAGE
+
+    def execute(self, user, provider_name, function_title, file_name) -> List[str]:
+        """
+        Get the dependencies from the whitlist
+        """
+        function = self.function_repository.get_function_by_permission(
+            user=user,
+            permission_name=RUN_PROGRAM_PERMISSION,
+            function_title=function_title,
+            provider_name=provider_name,
+        )
+
+        if not function:
+            if provider_name:
+                error_message = f"Qiskit Function {provider_name}/{function_title} doesn't exist."  # pylint: disable=line-too-long
+            else:
+                error_message = f"Qiskit Function {function_title} doesn't exist."
+            raise NotFoundError(error_message)
+
+        file_storage = FileStorage(
+            username=user.username,
+            working_dir=self.working_dir,
+            function_title=function_title,
+            provider_name=provider_name,
+        )
+        result = file_storage.remove_file(file_name=file_name)
+
+        if not result:
+            raise NotFoundError("Requested file was not found.")
+
+        return result
