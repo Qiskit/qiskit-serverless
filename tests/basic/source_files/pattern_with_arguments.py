69319e51ab1715192ba8b3e6ec0d5f03919d281e
Anay-Verma <Anay.Verma@ibm.com>
2025-08-12 14:32:13 +0530

    Added Test

--- tests/basic/source_files/pattern_with_arguments.py
+++ tests/basic/source_files/pattern_with_arguments.py
@@ -1,20 +1,23 @@
-# source_files/program_3.py
-
 from qiskit_serverless import get_arguments, save_result
+from qiskit import QuantumCircuit
+from qiskit_ibm_runtime import SamplerV2 as Sampler
+from qiskit_ibm_runtime.fake_provider import FakeAlmadenV2
+from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
 
-from qiskit.primitives import StatevectorSampler as Sampler
-
-# get all arguments passed to this program
+# Get the input circuit from arguments and set up a simulated backend.
 arguments = get_arguments()
-
-# get specific argument that we are interested in
 circuit = arguments.get("circuit")
+backend = FakeAlmadenV2()
 
-sampler = Sampler()
+# Transpile the circuit to one optimized for the backend's hardware.
+pm = generate_preset_pass_manager(optimization_level=1, backend=backend)
+isa_qc = pm.run(circuit)
 
-quasi_dists = sampler.run([(circuit)]).result()[0].data.meas.get_counts()
+# Run the transpiled circuit on the sampler and get the measurement counts.
+sampler = Sampler(mode=backend)
+job = sampler.run([isa_qc], shots=1024)
+result = job.result()[0].data.meas.get_counts()
+print("\nMeasurement counts:", result)
 
-print(f"Quasi distribution: {quasi_dists}")
-
-# saving results of a program
-save_result({"quasi_dists": quasi_dists})
+# Save the final result for the serverless job.
+save_result(result)
--- tests/basic/source_files/pattern_with_parallel_workflow.py
+++ tests/basic/source_files/pattern_with_parallel_workflow.py
@@ -1,26 +1,41 @@
-# source_files/program_with_parallel_workflow.py
+# program.py
 
 from qiskit_serverless import get_arguments, save_result, distribute_task, get
 
 from qiskit import QuantumCircuit
-from qiskit.primitives import StatevectorSampler as Sampler
-from qiskit.circuit.random import random_circuit
+from qiskit_ibm_runtime import SamplerV2 as Sampler
+from qiskit_ibm_runtime.fake_provider import FakeAlmadenV2
+from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
 
-
+# The @distribute_task decorator marks this function to be executed on a remote worker.
+# It transpiles a circuit for a specific backend, runs it, and returns the results.
 @distribute_task()
-def distributed_sample(circuit: QuantumCircuit):
-    """Distributed task that returns quasi distribution for given circuit."""
-    return Sampler().run([(circuit)]).result()[0].data.meas.get_counts()
+def distributed_compile_and_sample(ideal_circuit: QuantumCircuit, shots: int):
+    """
+    A distributed task that compiles a circuit for a specific backend ISA,
+    runs it, and returns the measurement counts.
+    """
+    backend = FakeAlmadenV2()
+    pm = generate_preset_pass_manager(optimization_level=1, backend=backend)
+    sampler = Sampler(mode=backend)
+    isa_circuit=pm.run(ideal_circuit)
+    pub_result = sampler.run([isa_circuit], shots=shots).result()[0]
+    return pub_result.data.meas.get_counts()
 
 
-arguments = get_arguments()
-circuits = arguments.get("circuits")
+if __name__ == "__main__":
+    # Get input arguments, which include a list of circuits to process.
+    arguments = get_arguments()
+    circuits = arguments.get("circuits")
+    shots = arguments.get("shots", 4096) 
 
-# run distributed tasks as async function
-# we get task references as a return type
-sample_task_references = [distributed_sample([(circuit)]) for circuit in circuits]
+    # Submit a remote task for each circuit. This returns immediately with references to the tasks.
+    task_references = [
+        distributed_compile_and_sample(circuit, shots) for circuit in circuits
+    ]
 
-# now we need to collect results from task references
-results = get(sample_task_references)
-
-save_result({"results": results})
+    # Wait for all the distributed tasks to complete and retrieve their results.
+    results = get(task_references)
+    
+    # Save the final aggregated results.
+    save_result({"results": results})
