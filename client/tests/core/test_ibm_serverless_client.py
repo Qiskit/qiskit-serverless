d83fcf26f17e367c617191eedc355949728417a0
ElePT <epenatap@gmail.com>
2025-08-28 16:44:42 +0200

    Fix lint

--- client/tests/core/test_ibm_serverless_client.py
+++ client/tests/core/test_ibm_serverless_client.py
@@ -16,8 +16,6 @@ import unittest
 import tempfile
 from unittest.mock import patch
 
-from qiskit_ibm_runtime.accounts.exceptions import InvalidAccountError
-
 from qiskit_serverless import IBMServerlessClient
 from qiskit_serverless.core.enums import Channel
 
