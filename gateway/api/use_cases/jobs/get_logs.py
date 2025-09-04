abaa5f8380a47494d6845659e4d645d7e091ef02
Pablo Aragón <pablo.aragon@ibm.com>
2025-09-04 10:51:36 +0200

    add get logs migration

--- gateway/api/use_cases/jobs/get_logs.py
+++ gateway/api/use_cases/jobs/get_logs.py
@@ -4,11 +4,23 @@ from api.domain.exceptions.not_found_error import NotFoundError
 
 
 class GetJobLogsUseCase:
+    """Use case for retrieving job logs."""
 
     jobs_repository = JobsRepository()
 
     def execute(self, job_id: str, user: AbstractUser) -> str:
+        """Return the logs of a job if the user has access.
 
+        Args:
+            job_id (str): Unique identifier of the job.
+            user (AbstractUser): User requesting the logs.
+
+        Raises:
+            NotFoundError: If the job does not exist.
+
+        Returns:
+            str: Job logs if accessible, otherwise a message indicating no logs are available.
+        """
         job = self.jobs_repository.get_job_by_id(job_id)
         if job is None:
             raise NotFoundError(f"Job [{job_id}] not found")
