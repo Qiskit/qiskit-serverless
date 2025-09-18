c021d436e986a4beadaf3f144b05338ab5e64ebe
ElePT <epenatap@gmail.com>
2025-09-18 14:34:30 +0200

    Catch warnings

--- tests/staging/source_files/pattern_with_runtime_wrapper.py
+++ tests/staging/source_files/pattern_with_runtime_wrapper.py
@@ -1,10 +1,17 @@
 # source_files/pattern_with_runtime_wrapper.py
 
 import os
+import warnings
 from qiskit_serverless import save_result, ServerlessRuntimeService
 
 from qiskit import QuantumCircuit
 from qiskit_ibm_runtime import SamplerV2
+
+warnings.filterwarnings(
+    "ignore",
+    message="Unable to create configuration for*",
+    category=UserWarning,
+)
 
 service = ServerlessRuntimeService(
     channel=os.environ["QISKIT_IBM_CHANNEL"],
@@ -12,6 +19,7 @@ service = ServerlessRuntimeService(
     token=os.environ["QISKIT_IBM_TOKEN"],
     url="https://test.cloud.ibm.com",
 )
+
 
 backends = service.backends()
 backend = service.backend("test_eagle")
@@ -24,7 +32,7 @@ out2 = sampler.run([qc])
 
 save_result(
     {
-        "backends": [back.name for back in backend],
+        "backends": [back.name for back in backends],
         "results": [out1.job_id, out2.job_id],
     }
 )
