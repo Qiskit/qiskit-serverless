7edc45033a5a6a4ccf0bde534d60e18b22bf2aac
ElePT <epenatap@gmail.com>
2025-10-02 10:21:50 +0200

    Adjust mock

--- client/tests/core/test_job.py
+++ client/tests/core/test_job.py
@@ -70,16 +70,12 @@ class ResponseMockWithRuntimeJobs:
 
     def json(self):
         "Serialize mock response"
-        return [
-            {
-                "runtime_job": "runtime_job_1",
-                "runtime_session": "session_id_1",
-            },
-            {
-                "runtime_job": "runtime_job_2",
-                "runtime_session": "session_id_2",
-            },
-        ]
+        return {
+            "runtime_jobs": [
+                {"runtime_job": "runtime_job_1", "runtime_session": "session_id_1"},
+                {"runtime_job": "runtime_job_2", "runtime_session": "session_id_2"},
+            ]
+        }
 
     @property
     def text(self):
