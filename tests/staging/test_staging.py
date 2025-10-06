ff9fcb2670b37925fe77a6968521ac78c63a9216
ElePT <epenatap@gmail.com>
2025-10-06 16:42:47 +0200

    Add second test for stop job

--- tests/staging/test_staging.py
+++ tests/staging/test_staging.py
@@ -2,7 +2,7 @@
 """Tests jobs."""
 import os
 
-
+from qiskit_ibm_runtime import QiskitRuntimeService
 from qiskit_serverless import (
     QiskitFunction,
     ServerlessClient,
@@ -48,6 +48,15 @@ class TestFunctionsStaging:
         assert runtime_job_ids == reference_job_ids
         assert session_ids == reference_session_ids
 
+        service = QiskitRuntimeService(
+            channel="ibm_quantum_platform",
+            token=os.environ["QISKIT_IBM_TOKEN"],
+            instance=os.environ["QISKIT_IBM_INSTANCE"],
+            url="https://test.cloud.ibm.com",
+        )
+
+        job.stop(service)
+
     def test_jobs_no_session(self, serverless_client: ServerlessClient):
         self._run_and_validate_function(
             serverless_client, "pattern_with_runtime_wrapper_1.py"
@@ -84,6 +93,46 @@ class TestFunctionsStaging:
 
         # Attempt to stop the job
         stop_response = serverless_client.stop(job_id)
+
+        # Validate the response
+        assert isinstance(stop_response, str)
+        assert "Job has been stopped" in stop_response
+        assert "QiskitRuntimeService not found" in stop_response
+        assert job.status() == "CANCELED"
+
+    def test_stop_job_service(self, serverless_client: ServerlessClient):
+        """Integration test for stopping a job given a runtime service."""
+
+        function = QiskitFunction(
+            title="test-runtime-wrapper",
+            entrypoint="pattern_with_stop.py",
+            working_dir=resources_path,
+            env_vars={
+                "QISKIT_IBM_CHANNEL": "ibm_quantum_platform",
+                "QISKIT_IBM_TOKEN": os.environ["QISKIT_IBM_TOKEN"],
+                "QISKIT_IBM_INSTANCE": os.environ["QISKIT_IBM_INSTANCE"],
+            },
+        )
+
+        serverless_client.upload(function)
+        my_function = serverless_client.function("test-runtime-wrapper")
+
+        job = my_function.run()
+        job_id = job.job_id
+
+        while True:
+            if "JOB IDS" in job.logs():
+                break
+
+        service = QiskitRuntimeService(
+            channel="ibm_quantum_platform",
+            token=os.environ["QISKIT_IBM_TOKEN"],
+            instance=os.environ["QISKIT_IBM_INSTANCE"],
+            url="https://test.cloud.ibm.com",
+        )
+
+        # Attempt to stop the job
+        stop_response = serverless_client.stop(job_id, service)
 
         # Validate the response
         assert isinstance(stop_response, str)
