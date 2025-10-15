499c3722556f5d444f4de9111c9c098a061cc735
David <9059044+Tansito@users.noreply.github.com>
2025-10-15 15:13:20 -0400

    allow to deactivate users

---
+++ gateway/api/access_policies/users.py
@@ -0,0 +1,35 @@
+"""
+Access policies implementation for Users access
+"""
+
+import logging
+from django.contrib.auth.models import AbstractUser
+
+
+logger = logging.getLogger("gateway")
+
+
+class UserAccessPolicies:
+    """
+    The main objective of this class is to manage the access for the user
+    to the application.
+    """
+
+    @staticmethod
+    def can_access(user: type[AbstractUser]) -> bool:
+        """
+        Checks if the user has access to the application.
+        For that it will need to be active.
+
+        Args:
+            user: Django user from the request
+
+        Returns:
+            bool: True or False in case the user has access
+        """
+
+        if user.is_active:
+            return True
+
+        logger.warning("User [%s] is not active.", user.username)
+        return False
--- gateway/tests/api/authentication/custom_token/test_ibm_quantum_platform.py
+++ gateway/tests/api/authentication/custom_token/test_ibm_quantum_platform.py
@@ -3,6 +3,7 @@
 
 from unittest.mock import MagicMock, patch
 import responses
+from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.test import APITestCase
 from ibm_platform_services import IamAccessGroupsV2, IamIdentityV1, ResourceControllerV2
 from ibm_cloud_sdk_core import DetailedResponse
@@ -14,6 +15,8 @@ from api.models import VIEW_PROGRAM_PERMISSION
 
 class TestIBMQuantumPlatformAuthentication(APITestCase):
     """This class contains e2e tests for IBM Quantum Platform authentication process."""
+
+    fixtures = ["tests/fixtures/authentication_fixtures.json"]
 
     @patch.object(IamAccessGroupsV2, "list_access_groups")
     @patch.object(ResourceControllerV2, "get_resource_instance")
@@ -104,3 +107,78 @@ class TestIBMQuantumPlatformAuthentication(APITestCase):
             for group in user.groups.all():
                 permissions = list(group.permissions.values_list("codename", flat=True))
                 self.assertEqual(permissions, [VIEW_PROGRAM_PERMISSION])
+
+    @patch.object(IamAccessGroupsV2, "list_access_groups")
+    @patch.object(ResourceControllerV2, "get_resource_instance")
+    @patch.object(IamIdentityV1, "get_api_keys_details")
+    @responses.activate
+    def test_default_authentication_workflow_with_inactive_account(
+        self,
+        mock_get_api_keys_details: MagicMock,
+        mock_get_resource_instance: MagicMock,
+        mock_list_access_groups: MagicMock,
+    ):
+        """
+        This test verifies the entire flow of the custom token authentication
+        for a not verified user.
+        """
+
+        mock_get_api_keys_details.return_value = DetailedResponse(
+            response={
+                "iam_id": "IBMid-1000000XYZ",
+                "account_id": "abc18abcd41546508b35dfe0627109c4",
+            },
+            headers={},
+            status_code=200,
+        )
+
+        mock_get_resource_instance.return_value = DetailedResponse(
+            response={
+                "resource_plan_id": "f04b2f00-35b0-46b0-b84d-eb63418417e6",
+            },
+            headers={},
+            status_code=200,
+        )
+
+        mock_list_access_groups.return_value = DetailedResponse(
+            response={
+                "groups": [
+                    {
+                        "id": "AccessGroupId-23afbcd24-00a0-00ab-ab0c-1a23b4c567de",
+                        "name": "Private Group",
+                    }
+                ],
+            },
+            headers={},
+            status_code=200,
+        )
+
+        responses.add(
+            responses.POST,
+            "https://iam.test.cloud.ibm.com/identity/token",
+            json={
+                "iam_id": "IBMid-1000000XYZ",
+                "account_id": "abc18abcd41546508b35dfe0627109c4",
+            },
+            status=200,
+        )
+
+        custom_auth = CustomTokenBackend()
+        request = MagicMock()
+        request.META = {
+            "HTTP_SERVICE_CHANNEL": "ibm_quantum_platform",
+            "HTTP_AUTHORIZATION": "Bearer AWESOME_TOKEN",
+            "HTTP_SERVICE_CRN": "AWESOME_CRN",
+        }
+
+        with self.settings(
+            IAM_IBM_CLOUD_BASE_URL="https://iam.test.cloud.ibm.com",
+            RESOURCE_CONTROLLER_IBM_CLOUD_BASE_URL="https://resource-controller.test.cloud.ibm.com",
+            RESOURCE_PLANS_ID_ALLOWED=["f04b2f00-35b0-46b0-b84d-eb63418417e6"],
+        ):
+            self.assertRaisesMessage(
+                AuthenticationFailed,
+                "Your user was deactivated. Please contact to support for reactivaton.",
+                custom_auth.authenticate,
+                request,
+            )
