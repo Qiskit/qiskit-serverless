ad2c1d1a3b5a0e1c6e4ac603149687b07a9bed6d
ItamarGoldman <itamargoldman@gmail.com>
2025-10-20 15:05:33 +0300

    fixed tutorial 4

--- docs/getting_started/basic/source_files/function_with_parallel_workflow.py
+++ docs/getting_started/basic/source_files/function_with_parallel_workflow.py
@@ -1,21 +1,25 @@
 from qiskit_serverless import get_arguments, save_result, distribute_task, get
 
 from qiskit import QuantumCircuit
-from qiskit.primitives import StatevectorSampler as Sampler
+from qiskit_ibm_runtime import SamplerV2 as Sampler
+from qiskit.transpiler import generate_preset_pass_manager
 
 
 @distribute_task()
-def distributed_sample(circuit: QuantumCircuit):
+def distributed_sample(circuit: QuantumCircuit, backend: BackendV2):
     """Distributed task that returns quasi distribution for given circuit."""
-    return Sampler().run([(circuit)]).result()[0].data.meas.get_counts()
+    pm = generate_preset_pass_manager(backend=backend, optimization_level=3)
+    isa_circuit = pm.run(circuit)
+    return Sampler(backend).run([isa_circuit]).result()[0].data.meas.get_counts()
 
 
 arguments = get_arguments()
 circuits = arguments.get("circuits")
+backend = arguments.get("backend")
 
 # run distributed tasks as async function
 # we get task references as a return type
-sample_task_references = [distributed_sample(circuit) for circuit in circuits]
+sample_task_references = [distributed_sample(circuit, backend) for circuit in circuits]
 
 # now we need to collect results from task references
 results = get(sample_task_references)
