3058c8956ff27276bb260715fdb578fba6eb4073
ItamarGoldman <itamargoldman@gmail.com>
2025-10-20 15:05:09 +0300

    fixed tutorial 1

--- docs/getting_started/basic/source_files/function.py
+++ docs/getting_started/basic/source_files/function.py
@@ -1,6 +1,7 @@
 from qiskit import QuantumCircuit
-from qiskit.primitives import StatevectorSampler as Sampler
-
+from qiskit.transpiler import generate_preset_pass_manager
+from qiskit_ibm_runtime import SamplerV2 as Sampler
+from qiskit_aer import AerSimulator
 from qiskit_serverless import save_result
 
 # all print statement will be available in job logs
@@ -12,11 +13,17 @@ circuit.h(0)
 circuit.cx(0, 1)
 circuit.measure_all()
 
-# running Sampler primitive
-sampler = Sampler()
-quasi_dists = sampler.run([(circuit)]).result()[0].data.meas.get_counts()
+# Using AerSimulator for the example
+backend = AerSimulator()
+pm = generate_preset_pass_manager(backend=backend, optimization_level=1)
+isa_circuit = pm.run(circuit)
 
-# saves results of function execution,
+# running Sampler primitive
+sampler = Sampler(backend)
+quasi_dists = sampler.run([isa_circuit]).result()[0].data.meas.get_counts()
+
+# save results of function execution, 
 # which will be accessible by calling `.result()`
 save_result(quasi_dists)
 print("Completed running function.")
+
