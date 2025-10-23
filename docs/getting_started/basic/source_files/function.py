16a1252e74bfc93bc46e03273e17a34ef5218c88
ItamarGoldman <itamargoldman@gmail.com>
2025-10-23 14:12:59 +0300

    changed the backend to be a fake backend instead of aer simulator.

--- docs/getting_started/basic/source_files/function.py
+++ docs/getting_started/basic/source_files/function.py
@@ -2,7 +2,7 @@
 from qiskit import QuantumCircuit
 from qiskit.transpiler import generate_preset_pass_manager
 from qiskit_ibm_runtime import SamplerV2 as Sampler
-from qiskit_aer import AerSimulator
+from qiskit_ibm_runtime.fake_provider import FakeVigoV2
 from qiskit_serverless import save_result
 
 # all print statement will be available in job logs
@@ -14,8 +14,8 @@ circuit.h(0)
 circuit.cx(0, 1)
 circuit.measure_all()
 
-# Using AerSimulator for the example
-backend = AerSimulator()
+# Using a fake backend for the example
+backend = FakeVigoV2()
 pm = generate_preset_pass_manager(backend=backend, optimization_level=1)
 isa_circuit = pm.run(circuit)
 
