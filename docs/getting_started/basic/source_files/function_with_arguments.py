4ac323696b642f0b34239425a171c866a3bdb88f
ItamarGoldman <itamargoldman@gmail.com>
2025-11-02 10:56:27 +0200

    Made things working with service

--- docs/getting_started/basic/source_files/function_with_arguments.py
+++ docs/getting_started/basic/source_files/function_with_arguments.py
@@ -1,7 +1,10 @@
+"""function with arguments for jupyter notebook."""
 from qiskit import QuantumCircuit
-from qiskit.providers import BackendV2
+from qiskit.providers.exceptions import QiskitBackendNotFoundError
 from qiskit.transpiler import generate_preset_pass_manager
 from qiskit_serverless import get_arguments, save_result
+from qiskit_ibm_runtime import QiskitRuntimeService
+from qiskit_ibm_runtime.fake_provider import FakeProviderForBackendV2
 from qiskit_ibm_runtime import SamplerV2 as Sampler
 
 
@@ -10,11 +13,24 @@ arguments = get_arguments()
 
 # get specific argument that we are interested in
 circuit = arguments.get("circuit")
-backend = arguments.get("backend")
+backend_name = arguments.get("backend_name")
+service = arguments.get("service")
 
 # verifying arguments types
-assert isinstance(circuit, QuantumCircuit)
-assert isinstance(backend, BackendV2)
+if not isinstance(circuit, QuantumCircuit):
+    raise ValueError("circuit must be QuantumCircuit.")
+if not isinstance(backend_name, str):
+    raise ValueError("backend_name must be str.")
+
+if "fake" in backend_name.lower():
+    service = FakeProviderForBackendV2()
+if isinstance(service, (FakeProviderForBackendV2, QiskitRuntimeService)):
+    try:
+        backend = service.backend(backend_name)
+    except QiskitBackendNotFoundError as e:
+        raise ValueError(f"Error retrieving backend {backend_name}: {e}") from e
+else:
+    raise ValueError(f"A service of type `QiskitRuntimeService` is required for using the backend named {backend_name}.")
 
 # matching our run to the backend argument
 sampler = Sampler(backend)
@@ -23,8 +39,6 @@ isa_circuit = pm.run(circuit)
 
 # running the circuit
 quasi_dists = sampler.run([isa_circuit]).result()[0].data.meas.get_counts()
-
-print(f"Quasi distribution: {quasi_dists}")
 
 # saving results of the execution
 save_result({
--- docs/getting_started/basic/source_files/function_with_parallel_workflow.py
+++ docs/getting_started/basic/source_files/function_with_parallel_workflow.py
@@ -3,8 +3,10 @@ from qiskit_serverless import get_arguments, save_result, distribute_task, get
 
 from qiskit import QuantumCircuit
 from qiskit.providers import BackendV2
+from qiskit.providers.exceptions import QiskitBackendNotFoundError
 from qiskit.transpiler import generate_preset_pass_manager
 from qiskit_ibm_runtime import SamplerV2 as Sampler
+from qiskit_ibm_runtime.fake_provider import FakeProviderForBackendV2
 
 
 @distribute_task()
@@ -17,11 +19,33 @@ def distributed_sample(circuit: QuantumCircuit, backend: BackendV2):
 
 arguments = get_arguments()
 circuits = arguments.get("circuits")
-my_backend = arguments.get("backend")
+backend_name = arguments.get("backend_name")
+service = arguments.get("service")
+
+# verifying arguments types
+if not isinstance(circuits, List):
+    circuits = [circuits]
+
+for circuit in circuits:
+    if not isinstance(circuit, QuantumCircuit):
+        raise ValueError("Each circuit in circuits must be QuantumCircuit.")
+if not isinstance(backend_name, str):
+    raise ValueError("backend_name must be str.")
+
+
+if "fake" in backend_name.lower():
+    service = FakeProviderForBackendV2()
+if isinstance(service, (FakeProviderForBackendV2, QiskitRuntimeService)):
+    try:
+        backend = service.backend(backend_name)
+    except QiskitBackendNotFoundError as e:
+        raise ValueError(f"Error retrieving backend {backend_name}: {e}") from e
+else:
+    raise ValueError(f"A service of type `QiskitRuntimeService` is required for using the backend named {backend_name}.")
 
 # run distributed tasks as async function
 # we get task references as a return type
-sample_task_references = [distributed_sample(circuit, my_backend) for circuit in circuits]
+sample_task_references = [distributed_sample(circuit, backend) for circuit in circuits]
 
 # now we need to collect results from task references
 results = get(sample_task_references)
