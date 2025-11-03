db0f7c7a1280d250fc9f15cbc67bb8bbf5694d4e
ItamarGoldman <itamargoldman@gmail.com>
2025-11-03 14:31:59 +0200

    updated tutorial 1 wording

--- docs/getting_started/basic/source_files/function.py
+++ docs/getting_started/basic/source_files/function.py
@@ -8,22 +8,22 @@ from qiskit_serverless import save_result
 # all print statement will be available in job logs
 print("Running function...")
 
-# creating circuit
+# Step 1: Create a Bell-state circuit
 circuit = QuantumCircuit(2)
 circuit.h(0)
 circuit.cx(0, 1)
 circuit.measure_all()
 
-# Using a fake backend for the example
+# Step 2: Transpile for a fake backend (for demonstration)
 backend = FakeVigoV2()
 pm = generate_preset_pass_manager(backend=backend, optimization_level=1)
 isa_circuit = pm.run(circuit)
 
-# running Sampler primitive
+# Step 3: Run the Sampler primitive
 sampler = Sampler(backend)
 quasi_dists = sampler.run([isa_circuit]).result()[0].data.meas.get_counts()
 
-# save results of function execution,
-# which will be accessible by calling `.result()`
+
+# Step 4: Save results so they can be retrieved later by calling `.result()`
 save_result(quasi_dists)
 print("Completed running function.")
