96d586be892bf7932e6f8191ab3e9da4bfae409d
korgan00 <korgan00@hotmail.com>
2025-12-04 17:23:01 +0100

    black

--- client/qiskit_serverless/utils/loggers.py
+++ client/qiskit_serverless/utils/loggers.py
@@ -1,5 +1,6 @@
 import logging
 
+
 class PrefixFormatter(logging.Formatter):
     def __init__(self, prefix, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -35,8 +36,7 @@ def _create_logger(name, prefix):
     if not logger.handlers:
         handler = logging.StreamHandler()
         formatter = PrefixFormatter(
-            prefix=prefix,
-            fmt="%(levelname)s:%(name)s: %(message)s"
+            prefix=prefix, fmt="%(levelname)s:%(name)s: %(message)s"
         )
         handler.setFormatter(formatter)
         logger.addHandler(handler)
