be6e896791f402598a0c4e6f3bd2825ec44f1fc8
korgan00 <korgan00@hotmail.com>
2025-12-04 17:33:08 +0100

    add docstrings

--- client/qiskit_serverless/utils/loggers.py
+++ client/qiskit_serverless/utils/loggers.py
@@ -1,12 +1,34 @@
+# This code is a Qiskit project.
+#
+# (C) Copyright IBM 2024.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""
+======================================================================
+Loggers utilities (:mod:`qiskit_serverless.utils.loggers`)
+======================================================================
+
+.. currentmodule:: qiskit_serverless.utils.loggers
+"""
 import logging
 
 
 class PrefixFormatter(logging.Formatter):
+    """Formater to add a prefix to logger and split in lines."""
+
     def __init__(self, prefix, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.prefix = prefix
 
     def format(self, record):
+        """format the record to add a prefix to logger and split in lines."""
         original_msg = record.getMessage()
         lines = original_msg.splitlines()
 
@@ -29,6 +51,7 @@ class PrefixFormatter(logging.Formatter):
 
 
 def _create_logger(name, prefix):
+    """creates a logger with the prefix formatter"""
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
     logger.propagate = False
@@ -45,8 +68,10 @@ def _create_logger(name, prefix):
 
 
 def get_logger():
+    """creates a logger for the user public logs"""
     return _create_logger("user", "[PUBLIC]")
 
 
 def get_provider_logger():
+    """creates a logger for the provider private logs"""
     return _create_logger("provider", "[PRIVATE]")
