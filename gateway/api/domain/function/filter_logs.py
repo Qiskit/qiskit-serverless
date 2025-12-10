ba8b469bf1d21bf6c17b139cd0ad8292d5015860
Goyo <korgan00@gmail.com>
2025-12-10 13:07:08 +0100

    Store logs on free resources (#1782)
    * store logs on freeresouces and modify endpoints to get directly from ray
    
    * added tests
    
    * black formatted
    
    * improve test
    
    * improve legibility
    Co-authored-by: avilches
    
    * improve test
    
    * improve tests
    
    * apply changes
    
    * black
    
    * fix lint
    
    * change import
    
    * fix ray file
    
    ---------
    
    Co-authored-by: Alberto Vilches <194074+avilches@users.noreply.github.com>

---
+++ gateway/api/domain/function/filter_logs.py
@@ -0,0 +1,17 @@
+"""Methods for filtering job logs."""
+import re
+
+
+def extract_public_logs(text: str) -> str:
+    """
+    This filter the logs to get the public ones only.
+
+    Args:
+        text: str
+
+    Return:
+        str -> The log filtered out
+    """
+    pattern = re.compile(r"^\[PUBLIC\]", re.IGNORECASE)
+    lines = [line[9:] for line in text.splitlines() if pattern.match(line)]
+    return "\n".join(lines) + "\n" if lines else ""
--- gateway/api/management/commands/free_resources.py
+++ gateway/api/management/commands/free_resources.py
@@ -5,8 +5,13 @@ import logging
 from django.conf import settings
 from django.core.management.base import BaseCommand
 
+from api.domain.function import check_logs
+from api.domain.function.filter_logs import extract_public_logs
 from api.models import ComputeResource, Job
-from api.ray import kill_ray_cluster
+from api.ray import kill_ray_cluster, get_job_handler
+from api.repositories.users import UserRepository
+from api.services.storage.enums.working_dir import WorkingDir
+from api.services.storage.logs_storage import LogsStorage
 
 
 logger = logging.getLogger("commands")
@@ -45,7 +50,7 @@ class Command(BaseCommand):
             if not there_are_alive_jobs:
                 self.remove_compute_resource(compute_resource)
 
-    def remove_compute_resource(self, compute_resource):
+    def remove_compute_resource(self, compute_resource: ComputeResource):
         """
         This method removes a Compute Resource if it's
         available in the cluster.
@@ -68,6 +73,9 @@ class Command(BaseCommand):
             )
             return
 
+        self.save_logs_to_storage(job=terminated_job, compute_resource=compute_resource)
+        terminated_job.logs = ""
+
         is_gpu = terminated_job.gpu
         should_remove_as_classical = remove_classical_jobs and not is_gpu
         should_remove_as_gpu = remove_gpu_jobs and is_gpu
@@ -83,3 +91,75 @@ class Command(BaseCommand):
                     compute_resource.title,
                     compute_resource.owner,
                 )
+
+    def save_logs_to_storage(self, job: Job, compute_resource: ComputeResource):
+        """
+        Save the logs in the corresponding storages.
+
+        Args:
+            compute_resource: ComputeResource
+            job: Job
+        """
+        job_handler = get_job_handler(compute_resource.host)
+        full_logs = job_handler.logs(job.ray_job_id)
+        full_logs = check_logs(full_logs, job)
+
+        user_repository = UserRepository()
+        author = user_repository.get_or_create_by_id(job.author)
+        username = author.username
+
+        has_provider = job.program.provider is not None
+
+        if has_provider:
+            self._save_logs_with_provider(full_logs, username, job)
+        else:
+            self._save_logs_only_user(full_logs, username, job)
+
+    def _save_logs_only_user(self, full_logs: str, username: str, job: Job):
+        """
+        Save the logs in the user storage.
+
+        Args:
+            full_logs: str
+            username: str
+            job: Job
+        """
+
+        user_logs_storage = LogsStorage(
+            username=username,
+            working_dir=WorkingDir.USER_STORAGE,
+            function_title=job.program.title,
+            provider_name=None,
+        )
+
+        user_logs_storage.save(job.id, full_logs)
+
+    def _save_logs_with_provider(self, full_logs: str, username: str, job: Job):
+        """
+        Save the logs in the provide storage and filter
+        for public logs only to save them into the user storage.
+
+        Args:
+            full_logs: str
+            username: str
+            job: Job
+        """
+
+        user_logs_storage = LogsStorage(
+            username=username,
+            working_dir=WorkingDir.USER_STORAGE,
+            function_title=job.program.title,
+            provider_name=None,
+        )
+
+        provider_logs_storage = LogsStorage(
+            username=username,
+            working_dir=WorkingDir.PROVIDER_STORAGE,
+            function_title=job.program.title,
+            provider_name=job.program.provider.name,
+        )
+
+        user_logs = extract_public_logs(full_logs)
+        user_logs_storage.save(job.id, user_logs)
+
+        provider_logs_storage.save(job.id, full_logs)
