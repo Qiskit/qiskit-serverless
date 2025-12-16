1721a78b89944856dc0c4631bef3f39365985311
Alberto Vilches <194074+avilches@users.noreply.github.com>
2025-12-16 09:52:20 -0500

    Move probes and version views to main folder (#1800)
    * move probes and version views to main folder
    
    * move probes and version views to main folder
    
    * moving files
    
    * adding health check
    
    * adding health check
    
    * removed __init__.py
    
    * removed comment
    
    * comments
    
    * comments

---
+++ gateway/api/views/probes.py
@@ -0,0 +1,25 @@
+"""Health check endpoints for Kubernetes readiness and liveness probes."""
+
+from django.db import connection, OperationalError
+from django.http import JsonResponse
+from api.apps import ApiConfig
+
+
+def readiness(request):
+    """Service is ready to accept traffic."""
+
+    if not ApiConfig.is_ready:
+        return JsonResponse({"status": "api_config_not_ready"}, status=503)
+
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT 1")
+    except OperationalError:
+        return JsonResponse({"status": "database_unavailable"}, status=503)
+
+    return JsonResponse({"status": "ready"})
+
+
+def liveness(request):
+    """Service is alive and running."""
+    return JsonResponse({"status": "alive"})
