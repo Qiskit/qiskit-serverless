5f3bb65b800c371eb3ada624ba4d9734269933fa
Alberto Vilches <alberto.vilches1@ibm.com>
2026-01-07 13:40:19 -0500

    fix

--- tests/docker/test_docker_runtime_staging.py
+++ tests/docker/test_docker_runtime_staging.py
@@ -2,7 +2,7 @@
 """Tests jobs using Qiskit Runtime's staging resources."""
 import os
 
-from _pytest import mark
+from pytest import mark
 from qiskit_ibm_runtime import QiskitRuntimeService
 from qiskit_serverless import (
     QiskitFunction,
