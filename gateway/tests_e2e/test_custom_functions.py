e7503351e0f1bc8b7158011dca1046efc922e639
Alberto Vilches <alberto.vilches1@ibm.com>
2026-02-05 20:23:52 -0500

    gateway e2e tests

---
+++ gateway/tests_e2e/test_custom_functions.py
@@ -0,0 +1,80 @@
+"""Custom jobs e2e tests"""
+
+from pytest import mark
+import json
+import requests
+
+
+class TestCustomFunction:
+
+    def test_custom_function_happy_path(self, client):
+        """Test complete lifecycle: upload -> get -> run -> status -> result -> logs"""
+        function_title = "test-custom-function"
+        entrypoint = "program.py"
+        arguments = {"message": "foo", "number": 42}
+
+        artifact_tar = client.create_artifact_tar(
+            """
+from qiskit_serverless import get_arguments, save_result
+save_result({ "arguments": get_arguments() })
+print("[test] Results saved.")
+""",
+            entrypoint,
+        )
+
+        response = client.upload(artifact_tar, entrypoint, function_title)
+        response.raise_for_status()
+
+        response = client.get_by_title(function_title)
+        response.raise_for_status()
+
+        response = client.list_programs()
+        response.raise_for_status()
+        assert any(p.get("title") == function_title for p in response.json())
+
+        response = client.run(function_title, arguments)
+        response.raise_for_status()
+        job_id = response.json()["id"]
+
+        response = client.list_jobs()
+        response.raise_for_status()
+        jobs = response.json()["results"]
+        assert any(p["program"]["title"] == function_title for p in jobs)
+
+        status = client.wait_for_status(job_id)
+        assert status == "SUCCEEDED"
+
+        response = client.get_logs(job_id)
+        response.raise_for_status()
+        print("--- Logs ---------------------------")
+        print(response.json()["logs"])
+        print("------------------------------------")
+        assert "[test] Results saved." in response.json()["logs"]
+
+        response = client.get_results(job_id)
+        response.raise_for_status()
+        assert json.loads(response.json()["result"])["arguments"] == arguments
+
+    def test_custom_function_broken(self, client):
+        """Test a function with a compiler error"""
+        function_title = "test-error-function"
+        entrypoint = "program.py"
+
+        artifact_tar = client.create_artifact_tar("""p r i n t ()""", entrypoint)
+
+        client.upload(artifact_tar, entrypoint, function_title)
+        response = client.run(function_title)
+        job_id = response.json()["id"]
+
+        status = client.wait_for_status(job_id)
+        assert status == "FAILED"
+
+        response = client.get_logs(job_id)
+        print("--- Logs ---------------------------")
+        print(response.json()["logs"])
+        print("------------------------------------")
+        assert "SyntaxError: invalid syntax" in response.json()["logs"]
+
+        response = client.get_results(job_id)
+        response.raise_for_status()
+        assert response.json()["result"] is None
---
+++ gateway/tests_e2e/test_smoke.py
@@ -0,0 +1,52 @@
+"""Smoke tests"""
+
+from pytest import mark
+import requests
+
+
+class TestSmokeTests:
+    def test_not_found(self, gateway_url):
+        response = requests.get(f"{gateway_url}/XXX", timeout=10)
+        assert response.status_code == 404
+
+    def test_swagger(self, gateway_url):
+        response = requests.get(f"{gateway_url}/swagger/", timeout=10)
+        response.raise_for_status()
+        assert "<title>Gateway API</title>" in response.text
+
+    @mark.skip(reason="TODO: specific User-Agent")
+    def test_backoffice(self, gateway_url):
+        response = requests.get(f"{gateway_url}/backoffice/login/", timeout=10)
+        response.raise_for_status()
+        assert "Django administration" in response.text
+
+    def test_auth_missing(self, gateway_url):
+        response = requests.get(f"{gateway_url}/api/v1/jobs/", timeout=10)
+        print(response.text)
+
+        assert response.json().get("detail") == "Authorization token was not provided."
+        assert response.status_code == 401
+
+    def test_jobs(self, client):
+        response = client.list_jobs()
+        print(response.text)
+        response.raise_for_status()
+
+        versions = response.json()
+        assert isinstance(versions["results"], list)
+
+    def test_program_list(self, client):
+        response = client.list_programs()
+        print(response.text)
+        response.raise_for_status()
+
+        programs = response.json()
+        assert isinstance(programs, list)
+
+    def test_dependencies_versions(self, client):
+        response = client.get_dependencies_versions()
+        print(response.text)
+        response.raise_for_status()
+
+        versions = response.json()
+        assert isinstance(versions, list)
