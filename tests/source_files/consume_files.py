37a6c987493e6daeb45a594e21a28a22804a73ca
Alberto Vilches <alberto.vilches1@ibm.com>
2026-02-16 10:29:39 -0500

    fix

--- tests/source_files/consume_files.py
+++ tests/source_files/consume_files.py
@@ -2,7 +2,8 @@ import os
 import tarfile
 from qiskit_serverless import save_result
 
-with tarfile.open("/data/my_file.tar", "r:gz") as tar:
+data_path = os.environ.get("DATA_PATH")
+with tarfile.open(f"{data_path}/my_file.tar", "r:gz") as tar:
     with tar.extractfile("./my_file.txt") as f:
         text = f.read().decode("utf-8")
 
