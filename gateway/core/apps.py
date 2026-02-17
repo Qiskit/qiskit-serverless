70dcaa094644a8c479c307b64ee65027eb407a0e
Alberto Vilches <alberto.vilches1@ibm.com>
2026-02-17 12:55:50 -0500

    ensure "core" models belongs to the api

--- gateway/core/apps.py
+++ gateway/core/apps.py
@@ -1,9 +1,10 @@
 """Django app configuration for the core module."""
 
-from django.apps import AppConfig
-from django.core.checks import Error, Tags, register
 import importlib
 import inspect
+
+from django.apps import AppConfig
+from django.core.checks import Error, Tags, register
 
 from django.db import models as django_models
 
@@ -20,7 +21,7 @@ class CoreConfig(AppConfig):
         register(Tags.models)(check_model_labels)
 
 
-def check_model_labels(app_configs, **kwargs):
+def check_model_labels(_app_configs, **kwargs):
     """
     This ensures all models have the same "api_" prefix in the table name and the
     migrations are located in the api app.
@@ -28,7 +29,7 @@ def check_model_labels(app_configs, **kwargs):
     errors = []
     core_models_module = importlib.import_module("core.models")
 
-    for name, obj in inspect.getmembers(core_models_module, inspect.isclass):
+    for _, obj in inspect.getmembers(core_models_module, inspect.isclass):
         if (
             not issubclass(obj, django_models.Model)
             or obj._meta.abstract
