bb9e9bc53acba60f4a654f0ec14869e3aed2a337
Alberto Vilches <alberto.vilches1@ibm.com>
2026-02-19 13:17:30 -0500

    Allow integration tests to runtime jobs with staging and production

--- tests/source_files/runtime_session.py
+++ tests/source_files/runtime_session.py
@@ -18,9 +18,7 @@ warnings.filterwarnings(
 )
 
 print(f"Function: QISKIT_IBM_INSTANCE: {os.environ.get('QISKIT_IBM_INSTANCE')}")
-print(
-    f"Function: QISKIT_IBM_TOKEN: {os.environ.get('QISKIT_IBM_TOKEN', '****')[:4]}****"
-)
+print(f"Function: QISKIT_IBM_TOKEN: {os.environ.get('QISKIT_IBM_TOKEN')[:4]}****")
 print(f"Function: QISKIT_IBM_URL: {os.environ.get('QISKIT_IBM_URL')}")
 print(f"Function: QISKIT_IBM_BACKEND_1: {os.environ.get('QISKIT_IBM_BACKEND_1')}")
 print(f"Function: QISKIT_IBM_BACKEND_2: {os.environ.get('QISKIT_IBM_BACKEND_2')}")
