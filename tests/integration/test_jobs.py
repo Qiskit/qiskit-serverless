89e858f4872d9db3e263f390a7a8101a7e4ad14c
Alberto Vilches <alberto.vilches1@ibm.com>
2026-02-20 12:53:06 -0500

    reduce tests

--- tests/integration/test_jobs.py
+++ tests/integration/test_jobs.py
@@ -348,26 +348,25 @@ ERROR: Provider log
 """.strip()
         )
 
+    def test_wrong_function_name(self, serverless_client: ServerlessClient):
+        """Integration test for retrieving a function that isn't accessible."""
 
-def test_wrong_function_name(self, serverless_client: ServerlessClient):
-    """Integration test for retrieving a function that isn't accessible."""
+        arguments_function = QiskitFunction(
+            title="pattern-with-arguments",
+            entrypoint="pattern_with_arguments.py",
+            working_dir=resources_path,
+        )
 
-    arguments_function = QiskitFunction(
-        title="pattern-with-arguments",
-        entrypoint="pattern_with_arguments.py",
-        working_dir=resources_path,
-    )
+        expected_message = (
+            "\n| Message: Http bad request.\n"
+            "| Code: 404\n"
+            "| Details: User program 'wrong-title' was not found or you do not "
+            "have permission to view it."
+        )
 
-    expected_message = (
-        "\n| Message: Http bad request.\n"
-        "| Code: 404\n"
-        "| Details: User program 'wrong-title' was not found or you do not "
-        "have permission to view it."
-    )
+        serverless_client.upload(arguments_function)
 
-    serverless_client.upload(arguments_function)
+        with raises(QiskitServerlessException) as exc_info:
+            serverless_client.function("wrong-title")
 
-    with raises(QiskitServerlessException) as exc_info:
-        serverless_client.function("wrong-title")
-
-    assert str(exc_info.value) == expected_message
+        assert str(exc_info.value) == expected_message
