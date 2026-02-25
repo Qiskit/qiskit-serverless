cc4a90301bd2030a4ec11f6e1ac121c3709b09b9
Junye Huang <h.jun.ye@gmail.com>
2026-02-25 14:57:17 +0100

    fix django migration conflict after merging main

--- gateway/api/migrations/0044_program_version.py
+++ gateway/api/migrations/0044_program_version.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("api", "0042_jobevent"),
+        ("api", "0043_config"),
     ]
 
     operations = [
