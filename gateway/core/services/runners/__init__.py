65b5eeab4a6cd90b46a694794dfe40cef940eb11
Alberto Vilches <alberto.vilches1@ibm.com>
2026-03-09 16:47:29 -0400

    client init

--- gateway/core/services/runners/__init__.py
+++ gateway/core/services/runners/__init__.py
@@ -1,10 +1,12 @@
-"""Runner clients for job execution backends."""
+"""Runner clients for job execution."""
 
-from core.services.runners.runner import Runner
-from core.services.runners.runner_client import RunnerError
+from core.models import Job
+from core.services.runners.runner_client import RunnerError, RunnerClient
+
+from core.services.runners.ray_client import RayClient
 
 
-def get_runner_client(job):
+def get_runner_client(job: Job) -> RunnerClient:
     """
     Factory: create the appropriate client for the job.
     The client is created with the job but does NOT connect automatically.
@@ -15,19 +17,9 @@ def get_runner_client(job):
     Returns:
         RunnerClient appropriate for the job's runner type
     """
-    # pylint: disable=import-outside-toplevel
-    # Imported here to avoid circular imports.
-    from core.services.runners.ray_client import RayClient
 
     # Future extension when job.runner is available:
     # from core.services.runners.fleets_client import FleetsClient
     # if job.runner == Runner.FLEETS:
     #     return FleetsClient(job)
     return RayClient(job)
-
-
-__all__ = [
-    "Runner",
-    "RunnerError",
-    "get_runner_client",
-]
