397ad9670f4ae417442962cdf740336fd6071970
korgan00 <korgan00@hotmail.com>
2026-03-17 13:21:36 +0100

    optional details

--- client/qiskit_serverless/core/serverless_error.py
+++ client/qiskit_serverless/core/serverless_error.py
@@ -19,11 +19,12 @@ from typing import Any
 class ServerlessError(Exception):
     """Base exception that can be used by function developers."""
 
-    def __init__(self, code: str, message: str, details: Any):
+    def __init__(self, code: str, message: str, details: Any | None):
         self.code = code
         self.message = message
         self.details = details
         super().__init__(self.message)
 
     def __str__(self):
-        return f"[{self.code}] {self.message}\n{self.details}"
+        details_str = f"\n{self.details}" if self.details else ""
+        return f"[{self.code}] {self.message}{details_str}"
