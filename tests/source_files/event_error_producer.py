557ad21642d6ba9abe8a9ddcb2755120cbe8a66b
korgan00 <korgan00@hotmail.com>
2026-03-18 13:33:02 +0100

    fix test

--- tests/source_files/event_error_producer.py
+++ tests/source_files/event_error_producer.py
@@ -3,4 +3,6 @@ from qiskit.primitives import StatevectorSampler as Sampler
 
 from qiskit_serverless import send_error
 
-send_error(1000, "My error message", {"my-arg-1": 123, "my-arg-2": "hi"})
+send_error(
+    code=1000, error_type="MyPersonalizedError", message="My error message", args={"my-arg-1": 123, "my-arg-2": "hi"}
+)
