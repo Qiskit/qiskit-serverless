d34530fbc476a0a11b550e13d1f89cec043e4903
Goyo <korgan00@gmail.com>
2026-03-25 19:02:10 +0100

    Refactor functions repository to query-set as manager (#1961)
    * Refactor functions repository to query-set as manager
    
    * remove commented code
    
    * better queries
    
    * remove unused function

--- gateway/api/use_cases/files/list.py
+++ gateway/api/use_cases/files/list.py
@@ -6,8 +6,9 @@ import logging
 from django.contrib.auth.models import AbstractUser
 
 from api.domain.exceptions.function_not_found_exception import FunctionNotFoundException
-from api.repositories.functions import FunctionRepository
+
 from core.models import RUN_PROGRAM_PERMISSION
+from core.models import Program as Function
 from core.services.storage.file_storage import FileStorage, WorkingDir
 
 logger = logging.getLogger("gateway.use_cases.files")
@@ -18,14 +19,13 @@ class FilesListUseCase:
     List all files on user storage use case.
     """
 
-    function_repository = FunctionRepository()
     working_dir = WorkingDir.USER_STORAGE
 
     def execute(self, user: AbstractUser, provider_name: str, function_title: str):
         """
         List all files on user storage.
         """
-        function = self.function_repository.get_function_by_permission(
+        function = Function.objects.get_function_by_permission(
             user=user,
             permission_name=RUN_PROGRAM_PERMISSION,
             function_title=function_title,
--- gateway/api/use_cases/files/provider_delete.py
+++ gateway/api/use_cases/files/provider_delete.py
@@ -9,9 +9,10 @@ from api.access_policies.providers import ProviderAccessPolicy
 from api.domain.exceptions.provider_not_found_exception import ProviderNotFoundException
 from api.domain.exceptions.function_not_found_exception import FunctionNotFoundException
 from api.domain.exceptions.file_not_found_exception import FileNotFoundException
-from api.repositories.functions import FunctionRepository
+
 from api.repositories.providers import ProviderRepository
 from core.models import RUN_PROGRAM_PERMISSION
+from core.models import Program as Function
 from core.services.storage.file_storage import FileStorage, WorkingDir
 
 logger = logging.getLogger("gateway.use_cases.files")
@@ -22,7 +23,6 @@ class FilesProviderDeleteUseCase:
     Delete file from provider storage use case.
     """
 
-    function_repository = FunctionRepository()
     provider_repository = ProviderRepository()
     working_dir = WorkingDir.PROVIDER_STORAGE
 
@@ -41,7 +41,7 @@ class FilesProviderDeleteUseCase:
         if provider is None or not ProviderAccessPolicy.can_access(user=user, provider=provider):
             raise ProviderNotFoundException(provider_name)
 
-        function = self.function_repository.get_function_by_permission(
+        function = Function.objects.get_function_by_permission(
             user=user,
             permission_name=RUN_PROGRAM_PERMISSION,
             function_title=function_title,
