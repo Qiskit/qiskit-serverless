e186937016e9c588289a5c361b529f26dfbe60ec
Alberto Vilches <alberto.vilches1@ibm.com>
2026-04-07 17:13:51 -0400

    added entrypoint files

--- gateway/entrypoint-scheduler.sh
+++ gateway/entrypoint-scheduler.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-until python manage.py migrate --check; do
-    echo "Waiting for migrations to be applied..."
-    sleep 1
-done
+python manage.py migrate_with_lock
 
 exec python manage.py run_scheduler
