7fa9f963d3a3aa2acbb67d258acf590c7cc0e0c8
Marcelo Amaral <marcelo.amaral@ibm.com>
2026-04-22 13:20:37 -0400

    Trim ce-swagger-sdk to only the models and APIs required at runtime
    Signed-off-by: Marcelo Amaral <marcelo.amaral@ibm.com>

--- gateway/ce-swagger-sdk/swagger_client/api/__init__.py
+++ gateway/ce-swagger-sdk/swagger_client/api/__init__.py
@@ -1,18 +1,6 @@
-# This code is a Qiskit project.
-#
-# (C) Copyright IBM 2026.
-#
-# This code is licensed under the Apache License, Version 2.0. You may
-# obtain a copy of this license in the LICENSE.txt file in the root directory
-# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
-#
-# Any modifications or derivative works of this code must retain this
-# copyright notice, and modified files need to carry a notice indicating
-# that they have been altered from the originals.
-
 from __future__ import absolute_import
 
 # flake8: noqa
 
-# import apis into api package
 from swagger_client.api.fleets_api import FleetsApi
+from swagger_client.api.secrets_and_configmaps_api import SecretsAndConfigmapsApi
