d0012a0e8c4fb75a1c12378b7ccd0457b6a97cac
Alberto Vilches <alberto.vilches1@ibm.com>
2026-04-28 14:32:26 -0400

    silence pylint unused-argument on base client stub

--- gateway/api/clients/function_access_client.py
+++ gateway/api/clients/function_access_client.py
@@ -6,6 +6,6 @@ from core.domain.authorization.function_access_result import FunctionAccessResul
 class FunctionAccessClient:
     """Client for retrieving accessible functions for a given instance CRN."""
 
-    def get_accessible_functions(self, instance_crn: str) -> FunctionAccessResult:
+    def get_accessible_functions(self, instance_crn: str) -> FunctionAccessResult:  # pylint: disable=unused-argument
         """Return all functions accessible to the given instance CRN with their actions."""
         return FunctionAccessResult(has_response=False)
