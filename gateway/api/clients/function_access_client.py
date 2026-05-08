d11fa8803f13a0f888d5f0c20d2f73db434a69d1
Alberto Vilches <alberto.vilches1@ibm.com>
2026-05-08 14:09:12 -0400

    fix

--- gateway/api/clients/function_access_client.py
+++ gateway/api/clients/function_access_client.py
@@ -61,6 +61,7 @@ class FunctionAccessClient:
             raise RuntimeFunctionsException(f"Unexpected status {response.status_code} for CRN {instance_crn}")
 
         functions = []
+        print(response.text)
         for entry in response.json().get("functions", []):
             try:
                 function_entry = FunctionAccessEntry(
