523913498b684e666cec5da964c28ee31fc5d4d0
Alberto Vilches <alberto.vilches1@ibm.com>
2026-05-22 14:08:19 -0400

    Enable Instances auth: user files endpoints

--- gateway/api/use_cases/files/delete.py
+++ gateway/api/use_cases/files/delete.py
@@ -8,7 +8,8 @@ from django.contrib.auth.models import AbstractUser
 from api.domain.exceptions.function_not_found_exception import FunctionNotFoundException
 from api.domain.exceptions.file_not_found_exception import FileNotFoundException
 
-from core.models import RUN_PROGRAM_PERMISSION
+from core.domain.authorization.function_access_result import FunctionAccessResult
+from core.models import PLATFORM_PERMISSION_USER_FILES_WRITE, RUN_PROGRAM_PERMISSION
 from core.models import Program as Function
 from core.services.storage.file_storage import FileStorage, WorkingDir
 
@@ -28,15 +29,18 @@ class FilesDeleteUseCase:
         provider_name: str,
         function_title: str,
         file_name: str,
+        accessible_functions: FunctionAccessResult,
     ):
         """
         Delete file from user storage.
         """
         function = Function.objects.get_function_by_permission(
             user=user,
-            legacy_permission_name=RUN_PROGRAM_PERMISSION,
             function_title=function_title,
             provider_name=provider_name,
+            accessible_functions=accessible_functions,
+            permission=PLATFORM_PERMISSION_USER_FILES_WRITE,
+            legacy_permission_name=RUN_PROGRAM_PERMISSION,
         )
 
         if not function:
