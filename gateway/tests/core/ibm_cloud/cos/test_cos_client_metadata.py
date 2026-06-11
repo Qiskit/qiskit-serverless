0712f63df5792f2fe395110a8d51b4e44db62d51
Alberto Vilches <alberto.vilches1@ibm.com>
2026-06-11 18:20:34 -0400

    fix test file: add copyright header, remove unused import

--- gateway/tests/core/ibm_cloud/cos/test_cos_client_metadata.py
+++ gateway/tests/core/ibm_cloud/cos/test_cos_client_metadata.py
@@ -1,7 +1,17 @@
-from unittest.mock import MagicMock, patch
-from datetime import datetime, timezone
+# This code is part of a Qiskit project.
+#
+# (C) IBM 2026
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
 
-import pytest
+from unittest.mock import MagicMock
+from datetime import datetime, timezone
 
 from core.ibm_cloud.cos.cos_client import COSClient, CosHmacCredentials
 
