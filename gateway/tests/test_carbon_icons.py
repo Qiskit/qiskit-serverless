41e9ae6a16070c2c53bc2c956806760ecb577604
Alberto Vilches <alberto.vilches1@ibm.com>
2026-06-12 15:11:43 -0400

    reorder INSTALLED_APPS so api/static shadows django.contrib.admin icons

---
+++ gateway/tests/test_carbon_icons.py
@@ -0,0 +1,16 @@
+"""Verifica que api/static sombrea los iconos de django.contrib.admin."""
+
+import pytest
+from django.contrib.staticfiles.finders import find
+
+
+@pytest.mark.django_db
+def test_api_icons_shadow_django_admin():
+    """Los iconos Carbon en api/static deben resolverse antes que los de Django."""
+    path = find("admin/img/icon-addlink.svg")
+    assert path is not None, "icon-addlink.svg no encontrado"
+    # Después de crear los SVGs en api/static, este assert debe pasar:
+    assert "api" in path, (
+        f"icon-addlink.svg se resolvió desde {path!r}, "
+        "esperaba que viniera de api/static (revisa el orden de INSTALLED_APPS)"
+    )
