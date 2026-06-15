e7e22ec28a7819dd5df28dda17cfbd93865e817d
Alberto Vilches <alberto.vilches1@ibm.com>
2026-06-15 17:12:34 -0400

    convert v1 programs view to package to allow endpoint submodules

--- gateway/api/v1/views/programs/__init__.py
+++ gateway/api/v1/views/programs/__init__.py
@@ -2,6 +2,7 @@
 Programs view api for V1.
 """
 
+from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import permissions, status
 from rest_framework.decorators import action
@@ -24,6 +25,10 @@ class ProgramViewSet(views.ProgramViewSet):
         return v1_serializers.JobConfigSerializer(*args, **kwargs)
 
     @staticmethod
+    def get_serializer_upload_program(*args, **kwargs):
+        return v1_serializers.UploadProgramSerializer(*args, **kwargs)
+
+    @staticmethod
     def get_serializer_run_program(*args, **kwargs):
         return v1_serializers.RunProgramSerializer(*args, **kwargs)
 
@@ -36,6 +41,31 @@ class ProgramViewSet(views.ProgramViewSet):
         return v1_serializers.JobSerializer(*args, **kwargs)
 
     @swagger_auto_schema(
+        operation_description="List author Qiskit Functions",
+        manual_parameters=[
+            openapi.Parameter(
+                "filter",
+                openapi.IN_QUERY,
+                description="Filters that you can apply for list: serverless, catalog or empty",
+                type=openapi.TYPE_STRING,
+                required=False,
+            ),
+        ],
+        responses={status.HTTP_200_OK: v1_serializers.ProgramSerializer(many=True)},
+    )
+    def list(self, request):
+        return super().list(request)
+
+    @swagger_auto_schema(
+        operation_description="Upload a Qiskit Function",
+        request_body=v1_serializers.UploadProgramSerializer,
+        responses={status.HTTP_200_OK: v1_serializers.UploadProgramSerializer},
+    )
+    @action(methods=["POST"], detail=False)
+    def upload(self, request):
+        return super().upload(request)
+
+    @swagger_auto_schema(
         operation_description="Run an existing Qiskit Function",
         request_body=v1_serializers.RunProgramSerializer,
         responses={status.HTTP_200_OK: v1_serializers.RunJobSerializer},
@@ -43,3 +73,26 @@ class ProgramViewSet(views.ProgramViewSet):
     @action(methods=["POST"], detail=False)
     def run(self, request):
         return super().run(request)
+
+    @swagger_auto_schema(
+        operation_description="Retrieve a Qiskit Function using the title",
+        manual_parameters=[
+            openapi.Parameter(
+                "title",
+                openapi.IN_PATH,
+                description="The title of the function",
+                type=openapi.TYPE_STRING,
+            ),
+            openapi.Parameter(
+                "provider",
+                openapi.IN_QUERY,
+                description="The provider in case the function is owned by a provider",
+                type=openapi.TYPE_STRING,
+                required=False,
+            ),
+        ],
+        responses={status.HTTP_200_OK: v1_serializers.ProgramSerializer},
+    )
+    @action(methods=["GET"], detail=False, url_path="get_by_title/(?P<title>[^/.]+)")
+    def get_by_title(self, request, title):
+        return super().get_by_title(request, title)
