7ce341005b1fc7cf3f5d4b7414cffc87c004cbd5
Alberto Vilches <alberto.vilches1@ibm.com>
2026-06-19 13:28:43 -0400

    move ProgramViewSet stub to v1/views/__init__.py; empty programs/__init__.py

--- gateway/api/v1/views/__init__.py
+++ gateway/api/v1/views/__init__.py
@@ -4,4 +4,17 @@ Django Rest framework ViewSets for api application:
 Specification for V1 version of the API.
 """
 
-from .programs import ProgramViewSet
+from rest_framework import permissions
+
+from api import views
+from api.v1 import serializers as v1_serializers
+
+
+class ProgramViewSet(views.ProgramViewSet):
+    """
+    Quantum function view set first version. Use ProgramSerializer V1.
+    """
+
+    serializer_class = v1_serializers.ProgramSerializer
+    pagination_class = None
+    permission_classes = [permissions.IsAuthenticated]
--- gateway/api/v1/views/programs/__init__.py
+++ gateway/api/v1/views/programs/__init__.py
@@ -1,18 +1,3 @@
 """
 Programs view api for V1.
 """
-
-from rest_framework import permissions
-
-from api import views
-from api.v1 import serializers as v1_serializers
-
-
-class ProgramViewSet(views.ProgramViewSet):
-    """
-    Quantum function view set first version. Use ProgramSerializer V1.
-    """
-
-    serializer_class = v1_serializers.ProgramSerializer
-    pagination_class = None
-    permission_classes = [permissions.IsAuthenticated]
