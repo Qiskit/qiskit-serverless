719fcb4cb6071071758b7c5bcc400f7bbc0e172a
Alberto Vilches <alberto.vilches1@ibm.com>
2026-06-23 23:53:10 -0400

    Inline UserRepository into AuthenticationUseCase

--- gateway/api/use_cases/authentication.py
+++ gateway/api/use_cases/authentication.py
@@ -1,53 +1,41 @@
 """Authentication use case to manage the authentication process in the api."""
 
 import logging
-from typing import Optional
+from typing import Optional, List
 
 from django.conf import settings
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from rest_framework import exceptions
 
 from api.access_policies.users import UserAccessPolicies
+from api.domain.authentication.authentication_group import AuthenticationGroup
 from api.domain.authentication.channel import Channel
-from api.repositories.users import UserRepository
 from api.services.authentication.authentication_base import AuthenticationBase
 from api.services.authentication.ibm_quantum_platform import IBMQuantumPlatform
 from api.services.authentication.local_authentication import LocalAuthenticationService
-from core.models import RUN_PROGRAM_PERMISSION, VIEW_PROGRAM_PERMISSION, Provider
+from core.models import GroupMetadata, RUN_PROGRAM_PERMISSION, VIEW_PROGRAM_PERMISSION, Provider
 
+User = get_user_model()
 logger = logging.getLogger("api.AuthenticationUseCase")
 
 
 class AuthenticationUseCase:
-    """
-    This class will manage the authentication flow for the api.
-    """
+    """This class will manage the authentication flow for the api."""
 
-    user_repository = UserRepository()
-
-    def __init__(
-        self,
-        channel: Channel,
-        authorization_token: str,
-        crn: Optional[str],
-        public_access=False,
-    ):
+    def __init__(self, channel, authorization_token, crn, public_access=False):
         self.channel = channel
         self.authorization_token = authorization_token
         self.crn = crn
         self.public_access = public_access
 
-    def _get_authentication_service_instance(self) -> AuthenticationBase:
+    def _get_authentication_service_instance(self):
         if self.channel in (Channel.IBM_CLOUD, Channel.IBM_QUANTUM_PLATFORM):
             return IBMQuantumPlatform(api_key=self.authorization_token, crn=self.crn)
-
         return LocalAuthenticationService(authorization_token=self.authorization_token)
 
-    def execute(self) -> tuple[Optional[type[AbstractUser]], Optional[str]]:
-        """
-        This contains the logic to authenticate and validate the user
-        that is doing the request.
-        """
+    def execute(self):
+        """This contains the logic to authenticate and validate the user."""
         authentication_service = self._get_authentication_service_instance()
 
         user_id = authentication_service.authenticate()
@@ -59,7 +47,9 @@ class AuthenticationUseCase:
                 raise exceptions.AuthenticationFailed("Sorry, you don't have access to the service.")
 
         access_groups = authentication_service.get_groups()
-        quantum_user = self.user_repository.get_or_create_by_id(user_id=user_id)
+        quantum_user, created = User.objects.get_or_create(username=user_id)
+        if created:
+            logger.debug("New user [%s] created", user_id)
         if not UserAccessPolicies.can_access(quantum_user):
             raise exceptions.AuthenticationFailed(
                 "Your user was deactivated. Please contact to IBM support for reactivaton."
@@ -67,7 +57,7 @@ class AuthenticationUseCase:
 
         if self.channel == Channel.LOCAL:
             permission_names = [VIEW_PROGRAM_PERMISSION, RUN_PROGRAM_PERMISSION]
-            groups = self.user_repository.restart_user_groups(
+            groups = self._restart_user_groups(
                 user=quantum_user,
                 authentication_groups=access_groups,
                 permission_names=permission_names,
@@ -81,10 +71,36 @@ class AuthenticationUseCase:
                     provider.admin_groups.add(admin_group)
         else:
             permission_names = [VIEW_PROGRAM_PERMISSION]
-            self.user_repository.restart_user_groups(
+            self._restart_user_groups(
                 user=quantum_user,
                 authentication_groups=access_groups,
                 permission_names=permission_names,
             )
 
         return quantum_user, account_id
+
+    def _restart_user_groups(
+        self,
+        user: type[AbstractUser],
+        authentication_groups: List[AuthenticationGroup],
+        permission_names: List[str],
+    ) -> List[Group]:
+        new_groups = []
+
+        permissions = []
+        for permission_name in permission_names:
+            permissions.append(Permission.objects.get(codename=permission_name))
+
+        user.groups.clear()
+
+        for authentication_group in authentication_groups:
+            group, created = Group.objects.get_or_create(name=authentication_group.group_name)
+            if created:
+                for permission in permissions:
+                    group.permissions.add(permission)
+            if authentication_group.account is not None:
+                GroupMetadata.objects.update_or_create(group=group, defaults={"account": authentication_group.account})
+            group.user_set.add(user)
+            new_groups.append(group)
+
+        return new_groups
