12eacb0e6bafddc475c4c2d0f21effa873d26a3c
Alberto Vilches <194074+avilches@users.noreply.github.com>
2026-09-10 08:14:47 +0000

    Revert "Exclude filler jobs from the Fleets status/timeout update task (#2473)" (#2475)
    This reverts commit b7aebf0d4bc67eac093c70855ea71db8f3ef696d.

--- gateway/scheduler/tasks/update_fleets_jobs_statuses.py
+++ gateway/scheduler/tasks/update_fleets_jobs_statuses.py
@@ -213,8 +213,7 @@ class UpdateFleetsJobsStatuses(SchedulerTask):
         # Note: with LIMITS_MAX_FLEETS potentially reaching 1000+ concurrent jobs, updating statuses
         # sequentially will become a bottleneck. This loop should be parallelized using multiple
         # threads or batched processing for performance reasons.
-        # Filler jobs are excluded: BalanceFillerJobs owns their full lifecycle
-        jobs = Job.objects.filter(status__in=Job.RUNNING_STATUSES, runner=Program.FLEETS, filler=False)
+        jobs = Job.objects.filter(status__in=Job.RUNNING_STATUSES, runner=Program.FLEETS)
         for job in jobs:
             if self.kill_signal.received:
                 logger.info("Kill signal received, stopping status update cycle")
