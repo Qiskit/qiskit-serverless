050a29d33dbf445d501369b17bd74a4162570bea
Alberto Vilches <alberto.vilches1@ibm.com>
2026-09-17 15:53:55 -0400

    Remove the save_model ordering test made obsolete by the reverted admin.py order

--- gateway/tests/test_job_admin_save_model.py
+++ gateway/tests/test_job_admin_save_model.py
@@ -17,26 +17,6 @@ def user():
 
 
 class TestSaveModelOrdering:
-    def test_persists_the_job_before_creating_the_status_event(self, user):
-        job = Job.objects.create(author=user, runner=Program.FLEETS, status=Job.QUEUED)
-        job.status = Job.STOPPED
-        form = MagicMock()
-        form.changed_data = ["status"]
-
-        call_order = []
-        original_save = Job.save
-
-        def spy_save(self, *args, **kwargs):
-            call_order.append("job_saved")
-            return original_save(self, *args, **kwargs)
-
-        with patch("api.admin.JobEvent") as mock_job_event:
-            mock_job_event.objects.add_status_event.side_effect = lambda **kw: call_order.append("event_created")
-            with patch.object(Job, "save", spy_save):
-                JobAdmin(Job, None).save_model(request=None, obj=job, form=form, change=True)
-
-        assert call_order == ["job_saved", "event_created"]
-
     def test_does_nothing_extra_when_status_unchanged(self, user):
         job = Job.objects.create(author=user, runner=Program.FLEETS, status=Job.QUEUED)
         form = MagicMock()
